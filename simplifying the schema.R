2e66d7cd84f45443ce5ee861c1e815b071e07746
benmiller314 <benmiller314@gmail.com>
2014-01-23 15:54:32 -0500

    update terms in 'simplifying the schema.R'
    not "collapse," but "simple"; include "other > tool-building" as part
    of "craft-based"

--- simplifying the schema.R
+++ simplifying the schema.R
@@ -2,27 +2,24 @@
 #  Possible groups: Aggregable (disc, expt, surv, meta), Phenomenological (case, ethn), Dialectical (crit, hist, modl, phil, rhet), Craft-Based (poet, prac [and tool-building]; cf. Johnson 2010). Bad fits: intv, meta. Drop cult and intv, move meta to Agg.
 # As an alternative pool, cf. Michael Carter's "Ways of Knowing and Doing in the Disciplines"?
 
-if(!exists(sfreq)) {
-	source(file="top 10 lists.R")
-}
-if(!exists(noexcludes)) {
+if(!exists("noexcludes")) {
 	source(file="dataprep.R")
 }
 
 # define shortcut for new tag names
-tagnames.collapse <- c("Aggreg", "Phenom", "Dialec", "Crafty")
+tagnames.simple <- c("Aggreg", "Phenom", "Dialec", "Crafty")
 	
 short_schema <- function (data) {	
 	# Create a data frame to hold the updated info; we'll merge later.
-	collapse <- data.frame(
+	simple <- data.frame(
 			Pub.number = data["Pub.number"],
 			Aggreg = -1,
 			Phenom = -1,
 			Dialec = -1,
 			Crafty = -1,
-			Counts.collapse = -1
+			Counts.simple = -1
 	)
-	head(collapse)
+	head(simple)
 	
 	# For each data row, record if it's tagged with any member of each group
 	for (i in 1:nrow(data)) {
@@ -32,13 +29,13 @@ short_schema <- function (data) {
 		a3 <- as.integer(data[i,"Surv"])
 		a4 <- as.integer(data[i,"Meta"])
 		a <- max(a1, a2, a3, a4)
-		ag <- collapse[i,"Aggreg"] <- a
+		ag <- simple[i,"Aggreg"] <- a
 		
 		# Phenomenological
 		a1 <- as.integer(data[i,"Case"])
 		a2 <- as.integer(data[i,"Ethn"])
 		a <- max(a1, a2)
-		ph <- collapse[i,"Phenom"] <- a
+		ph <- simple[i,"Phenom"] <- a
 		
 		# Dialectical
 		a1 <- as.integer(data[i,"Crit"])
@@ -47,35 +44,37 @@ short_schema <- function (data) {
 		a4 <- as.integer(data[i,"Phil"])
 		a5 <- as.integer(data[i,"Rhet"])
 		a <- max(a1, a2, a3, a4, a5)
-		di <- collapse[i,"Dialec"] <- a
+		di <- simple[i,"Dialec"] <- a
 		
 		# Craft-Based
 		a1 <- as.integer(data[i,"Poet"])
 		a2 <- as.integer(data[i,"Prac"])
-		a <- max(a1, a2)
-		cr <- collapse[i,"Crafty"] <- a
+		a3 <- as.integer(grep("tool-building",data[i,"Method.Terms"],ignore.case=TRUE))
+		a <- max(a1, a2, a3)
+		cr <- simple[i,"Crafty"] <- a
 		
 		# Now look for multi-modality across these broad categories
-		collapse[i, "Counts.collapse"] <- sum(ag, ph, di, cr)
+		simple[i, "Counts.simple"] <- sum(ag, ph, di, cr)
 	}
 	# Clean up the workspace
 	rm(a, a1, a2, a3, a4, a5, ag, ph, di, cr)
 
-	return(collapse)
+	return(simple)
 }
 
 # # Confirm the function works properly
-# collapse <- short_schema(noexcludes)
-# head(collapse, 10)
+# simple <- short_schema(bigarray)
+# head(simple, 10)
+
 	
 # # Explore the newly configured data
-# table(collapse$Counts.collapse)
-# mean(collapse$Counts.collapse)
-# noexcludes[which(collapse$Counts == 0),c("Method.Terms",tagnames)]
+# table(simple$Counts.simple)
+# mean(simple$Counts.simple)
+# noexcludes[which(simple$Counts == 0),c("Method.Terms",tagnames)]
 
 # names(noexcludes)
 
-# colSums(collapse[2:(ncol(collapse)-1)])
+# colSums(simple[2:(ncol(simple)-1)])
 
 
 
