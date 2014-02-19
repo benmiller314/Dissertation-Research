b0a87549315c47a48e7c318bddd634f93483b357
benmiller314 <benmiller314@gmail.com>
2014-02-19 12:30:55 -0500

    in data function of tags-by-school, get $means and $sums

--- tags by school.R
+++ tags by school.R
@@ -13,25 +13,27 @@ if(!exists("imageloc")) {
 myCol <- brewer.pal(9, "PuRd")
 
 # function for getting data
-schoolwise.data <- function(dataset_name, tagset_name) {
+schoolwise.data <- function(dataset_name="consorts", tagset_name="tagnames") {
 	
 	# 0. convert variable names to variables. we'll use the names later in the figure titles.
 	dataset <- get(dataset_name)
 	tagset <- get(tagset_name)
-	tagset <- sapply(tagset, FUN=function(x) paste0(x,".mean"))
+	tagset.mean <- sapply(tagset, FUN=function(x) paste0(x,".mean"))
+	tagset.sum <- sapply(tagset, FUN=function(x) paste0(x,".sum"))
 	
-	# 1. do the summary (tag mean) of each method type for all schools. 
-	d1 <- summaryBy(. ~ School, data=dataset)
+	# 1. remove columns other than method tags and school
+	dataset <- dataset[, which(names(dataset) %in% c("School", tagset))]
+
+	# 2. do the summary (tag mean) of each method type for all schools. 
+	d1 <- summaryBy(. ~ School, data=dataset, FUN=mean)
+	d2 <- summaryBy(. ~ School, data=dataset, FUN=sum)
 	
-	# 2. get more meaningful row names (and a purely numerical matrix, for heatmapping)
-	# Note that the first column will always be the list of schools because of the query in step 1.
-	row.names(d1) <- d1[,1]
+	# 3. get more meaningful row names (and a purely numerical matrix, for heatmapping)
+	# Note that the first column will always be the list of schools because of the query in step 2.
+	row.names(d1) <- d1[,1]; d1 <- d1[,2:ncol(d1)]
+	row.names(d2) <- d2[,1]; d2 <- d2[,2:ncol(d2)]
 	
-	# 3. remove columns other than method tags, again for the heatmap, and matricize	
-	m1 <- d1[,which(names(d1) %in% tagset)]
-	m2 <- data.matrix(m1)
-	
-	return(m2)
+	return(list("means" = d1, "sums" = d2))
 }
 
 # function for graphing data
@@ -43,7 +45,7 @@ schoolwise <- function(dataset_name, tagset_name, agfixedcols=NULL, difixedcols=
 	tagset <- sapply(tagset, FUN=function(x) paste0(x,".mean"))
 	
 	# 1-3 call the data-grabbing function
-	m2 <- schoolwise.data(dataset_name, tagset_name)
+	m2 <- schoolwise.data(dataset_name, tagset_name)$means
 	
 	# 4. make the heatmap: use pre-determined columns if need be.
 
@@ -75,7 +77,7 @@ schoolwise <- function(dataset_name, tagset_name, agfixedcols=NULL, difixedcols=
 	
 
 	# save the row and column orders to allow for consistent sorting later
-	return(list("ag" = ag, "di" = di, byschool = m1))
+	return(list("ag" = ag, "di" = di, byschool = m2))
 	
 # close wrapper function
 }
@@ -90,15 +92,15 @@ schoolwise("noexcludes", "tagnames", agfixedcols=a$ag$colInd, difixedcols=a$di$c
 b <- schoolwise("consorts", "tagnames.simple")
 schoolwise("nonconsorts", "tagnames.simple")
 c <- schoolwise("noexcludes", "tagnames.simple")
-schoolwise("consorts", "tagnames.simple", agfixedcols=c$ag$colInd, difixedcols=c$di$colInd)
-schoolwise("nonconsorts", "tagnames.simple", agfixedcols=c$ag$colInd, difixedcols=c$di$colInd)
+# schoolwise("consorts", "tagnames.simple", agfixedcols=c$ag$colInd, difixedcols=c$di$colInd)
+# schoolwise("nonconsorts", "tagnames.simple", agfixedcols=c$ag$colInd, difixedcols=c$di$colInd)
 
-# explore the data
-d <- order(c$byschool$Aggreg.mean, decreasing=TRUE)
-schoolwise("consorts", "tagnames.simple", agfixedcols=d, difixedcols=d)
+# # explore the data
+# d <- order(c$byschool$Aggreg.mean, decreasing=TRUE)
+# schoolwise("consorts", "tagnames.simple", agfixedcols=d, difixedcols=d)
 
-c$byschool[c$di$rowInd, which(names(c$byschool) %in% sapply(tagnames.simple, FUN=function(x) paste0(x,".mean")))]
-which(rowsum(c$byschool, row.names(c$byschool)) == 0)
-?rowsum
-# remove interim variables
-rm(m1, m2, m3, noex.by.school.m, nonconsorts.by.school.m, consorts.by.school.m)
+# c$byschool[c$di$rowInd, which(names(c$byschool) %in% sapply(tagnames.simple, FUN=function(x) paste0(x,".mean")))]
+# which(rowsum(c$byschool, row.names(c$byschool)) == 0)
+# ?rowsum
+# # remove interim variables
+# rm(m1, m2, m3, noex.by.school.m, nonconsorts.by.school.m, consorts.by.school.m)
