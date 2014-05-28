34730983002666bc51054dbb0ff7dacfd13062b1
benmiller314 <benmiller314@gmail.com>
2014-05-28 10:52:19 -0400

    cleaned up old code
    a lot of it

--- collocation heatmap.R
+++ collocation heatmap.R
@@ -5,6 +5,7 @@
 #  the number of times a dissertation tagged X is *only* tagged X.
 #  The total number of times dissertations are tagged X is returned separately.
 
+#  1. Calculate tag collocations, total dissertations per tag, and solo counts per tag.
 sumbytags <- function(dataset_name="noexcludes", tagset_name="tagnames") {
 	dataset <- get(dataset_name)
 	tagset <- get(tagset_name)
@@ -45,139 +46,26 @@ sumbytags <- function(dataset_name="noexcludes", tagset_name="tagnames") {
 				 "solo.counts" = solo.counts,
 				 "total.counts" = total.counts))
 }
+
+# 1a. Run it when the file is called
 sum.by.tags <- sumbytags()
 
-
-# ## Make a horizontal bar plot of total dissertation counts for each tag.
-# total.counts.barplot <- function (dataset_name="noexcludes", tagset_name="tagnames") {
-	# sum.by.tags <- sumbytags(dataset_name, tagset_name)
-	# total.counts <- sort(sum.by.tags$total.counts, decreasing=FALSE)
-	# length <- length(tagnames)
-	# diss.count <- nrow(get(dataset_name))
-
-	# main <- "Frequency of Assigned Method Tags"
-	# submain <- paste0(dataset_name, ", ", tagset_name)
-	# if(remake_figs) { 
-			# filename <- paste0(imageloc, main, ", N", diss.count, ".pdf")
-			# pdf(filename) 
-	# }
-		# barplot(total.counts, horiz=TRUE, xpd=FALSE, las=1, axes=FALSE)
-		# title(main)
-		# text(x=total.counts-30, y=seq(from=0.7,to=(length+2.5),length.out=length), labels=total.counts)
-		# mtext(submain, side=3)
-		# mtext(paste("Tags are non-exclusive, so sum will be greater than the", diss.count, "dissertations."), side=1)
-
-	# if(remake_figs) { dev.off() }
-# }
-# total.counts.barplot()
-
-# ## Make a horizontal bar plot of solo dissertation counts for each tag.
-# solo.counts.barplot <- function (dataset_name="noexcludes", tagset_name="tagnames") {
-	# sum.by.tags <- sumbytags(dataset_name, tagset_name)
-	# solo.counts <- sort(sum.by.tags$solo.counts, decreasing=FALSE)
-	# length <- length(tagnames)
-	# diss.count <- nrow(get(dataset_name))
-
-	# main <- "Frequency of Exclusively Assigned Method Tags"
-	# submain <- paste0(dataset_name, ", ", tagset_name)
-	# if(remake_figs) { 
-			# filename <- paste0(imageloc, main, ", N", diss.count, ".pdf")
-			# pdf(filename) 
-	# }
-		# barplot(solo.counts, horiz=TRUE, xpd=FALSE, las=1, axes=FALSE)
-		# title(main)
-		# text(x=solo.counts-5, y=seq(from=0.7,to=(length+2.5),length.out=length), labels=solo.counts)
-		# mtext(submain, side=3)
-		# mtext(paste("For the subset of ", sum(solo.counts), "of", diss.count, "dissertations with only one tag."), side=1)
-
-	# if(remake_figs) { dev.off() }
-# }
-# solo.counts.barplot()
-
-# TO DO: Remake the heatplot function given the new summary function above
-
-# now let's see how that looks as a heatmap...
-require(RColorBrewer)
-
-if(remake_figs) { 
-	filename <- paste0(imageloc, "Method Tag Co-Occurrence (R heatmap function), N",diss.count,".pdf")
-	pdf(filename) 
-}
-
-h1 <- heatmap(sum.by.tags$correlations, symm=TRUE, main="Method Tag Co-Occurrence", sub="raw counts; diagonals are solo methods", col=brewer.pal(9,"YlOrRd"))
-
-if(remake_figs) { dev.off() }
-
-# can I customize that?
-sum.by.tags.s <- sum.by.tags2[h1$rowInd,h1$colInd] 		# sorted by similarity function in heatmap
-
-# heatmap.ben function now saved in 'heatmap_ben.R', so it loads during dataprep
-heatmap(sum.by.tags$correlations)
-heatmap.ben(sum.by.tags, diags=T)
-
-# give it a spin!
+# 2. Basic heatmap
 if(remake_figs) {
 	pdf(filename) 
-	filename <- paste0(imageloc, "Method Tag Co-Occurrence (ben heatmap), N", diss.count, ".pdf")
+	filename <- paste0(imageloc, "Method Tag Co-Occurrence, N", diss.count, ".pdf")
 } 
-	heatmap.ben(sum.by.tags$correlations, diags=TRUE)
-	title(main="Method Tag Co-Occurrence", sub="diagonals are solo methods")
+	heatmap.ben(sum.by.tags, diags=TRUE)
+	title(main="Method Tag Co-Occurrence")
 	mtext("A box in row X, column Y gives the number of \n dissertations tagged Y that are also tagged X", side=2)
 if(remake_figs) { dev.off() }
 
-
-
-# The most frequent tags hide other collocations, so let's norm down each column (which it weirdly thinks are the rows... some day, I'll figure out why --> UPDATE: it was just mislabeled b/c of i=x and j=y.)
-sum.by.tags.n <- sum.by.tags.s
-for (i in 1:nrow(sum.by.tags.n)) {
-	# find the total to divide out by
-	index <- names(sum.by.tags.n)[i]
-	value <- tag.totals[which(index == names(tag.totals))]
-	
-	# divide
-	sum.by.tags.n[i,] <- round((sum.by.tags.n[i,] / value), digits=4)
+# 3. Normed heatmap
+if(remake_figs) { 
+	filename <- paste0(imageloc, "Method Tag Co-Occurrence (normed by row), N", diss.count,".pdf")
+	pdf(filename) 
 }
-
-# to reduce clutter, I want to give these as whole-number percentages
-sum.by.tags.n <- round(sum.by.tags.n * 100, 0)
-print(sum.by.tags.n)
-
-filename <- paste0(imageloc, "Method Tag Co-Occurrence (ben heatmap, normed, whole numbers), N",diss.count,".pdf")
-if(remake_figs) { pdf(filename) }
-	# heatmap(data.matrix(sum.by.tags.n),symm=TRUE,main="Method Tag Co-Occurrence", sub="normed by dividing intersection count by row total;\n diagonals are solo methods", col=brewer.pal(9,"YlOrRd"))
-	heatmap.ben(sum.by.tags.n)
-	title(main="Method Tag Co-Occurrence", sub="normed by dividing intersection count by row total;\n diagonals are solo methods")
+	heatmap.ben(sum.by.tags, rowscale=TRUE, diags=TRUE)
+	title(main="Method Tag Co-Occurrence")
 	mtext("A box in row X, column Y gives the probability \n that a dissertation tagged X is also tagged Y", side=2)
 if(remake_figs) { dev.off() }
-
-
-
-# Old approach: Given a csv with method tag collocation data (e.g. as constructed from Google Refine), construct a heat plot.
-# To do: figure out how to do the collocation in R.
-
-# # Starts with data prep, as always:
-# # filename <- readline("What's the name of the method tag collocation csv? ")
-# # read.csv(file=filename) -> method.col
-# read.csv(file="../method tag collocation noexcludes (N 1901).csv") -> method.col
-
-# names(method.col)[1] -> subtitle
-
-# row.names(method.col) <-method.col[,1]			# row names are in the first column
-# method.col <- method.col[,2:ncol(method.col)]	# we can dump that column
-# data.matrix(method.col) -> method.col.m			# heatmap needs a matrix, not a list
-
-
-# # now let's do it all again with the data normed within each tag row
-# read.csv(file="../method tag collocation normed within tag noexcludes (N 1901).csv") -> method.col.normed
-
-# # names(method.col.normed)[1] -> subtitle.normed
-# "Each cell (X,Y) gives the likelihood that a diss tagged X is also tagged Y" -> subtitle.normed
-
-# row.names(method.col.normed) <-method.col.normed[,1]
-# method.col.normed <- method.col.normed[,2:ncol(method.col.normed)]
-# data.matrix(method.col.normed) -> method.col.normed.m
-
-# # and finally, let's plot these babies:
-# par(mfrow=c(1,2))
-# heatmap(method.col.m,symm=TRUE,main="Method Tag Co-Occurrence",sub=subtitle, col=brewer.pal(9,"YlOrRd"))
-# heatmap(method.col.normed.m,symm=TRUE,main="Method Tag Co-Occurrence, normed by tag",sub=subtitle.normed,col=brewer.pal(9,"YlOrRd"))
