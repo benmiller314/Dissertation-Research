e5a04b8125977f4962bb3c31dd43b24c0bb47895
benmiller314 <benmiller314@gmail.com>
2014-06-26 15:08:06 -0400

    customized for my own needs

--- r2mallet.R
+++ r2mallet.R
@@ -6,49 +6,59 @@
 # forked again, and currently, by Ben Miller (https://github.com/benmiller314)
 #
 
-# Set working directory
-dir <- "~/mallet/tutorial" # adjust to suit
-setwd(dir)
+# Set working environment
+if (!exists(sourceloc)) {
+	source("/Users/benmiller314/Dropbox/coursework, etc/dissertation/R experiments/Dissertation Research/dataprep.R")
+}
+setwd(sourceloc)
+
+# folder containing txt files for MALLET to work on
+importdir <- "/Users/benmiller314/Documents/fulltext_dissertations/clean_consorts_only"
+
+# set number of topics for MALLET to use
+ntopics <- 10
+
+# setup system enviroment for R
+MALLET_HOME <- "/Users/benmiller314/mallet-2.0.7"			# location of the bin directory
+
+# Let's assume we're typically going to need more Java heap space;
+# this sets the maximum allocation
+    heap_param <- paste("-Xmx","2g",sep="") 
+    options(java.parameters=heap_param)
 
 # configure variables and filenames for MALLET
 ## here using MALLET's built-in example data and
 ## variables from http://programminghistorian.org/lessons/topic-modeling-and-mallet
 
-# folder containing txt files for MALLET to work on
-importdir <- "~/mallet/sample-data/web/en"
+
 # name of file for MALLET to train model on
-output <- "tutorial.mallet"
-# set number of topics for MALLET to use
-ntopics <- 20
+output <- "test.mallet"
 # set optimisation interval for MALLET to use
 optint <-  20
 # set file names for output of model, extensions must be as shown
 outputstate <-  "topic-state.gz"
-outputtopickeys <- "tutorial_keys.txt"
-outputdoctopics <- "tutorial_composition.txt"
+outputtopickeys <- "test_keys.txt"
+outputdoctopics <- "test_composition.txt"
   
-# combine variables into strings ready for windows command line
-cd <- "cd ~/mallet" # location of the bin directory
-import <- paste("bin/mallet import-dir --input", importdir, "--output", output, "--keep-sequence --remove-stopwords", sep = " ")
-train  <- paste("bin/mallet train-topics  --input", output, "--num-topics", ntopics, "--optimize-interval",  optint, "--output-state", outputstate,  "--output-topic-keys", outputtopickeys, "--output-doc-topics", outputdoctopics, sep = " ")
+# combine variables into strings ready for MacOS Terminal command line
+mallet_cmd <- paste0(MALLET_HOME, "/bin/mallet")
+cd <- paste("cd", shQuote(MALLET_HOME)) 				
+import <- paste(mallet_cmd, "import-dir --input", importdir, "--output", output, "--keep-sequence --remove-stopwords", sep = " ")
+train  <- paste(mallet_cmd, "train-topics  --input", output, "--num-topics", ntopics, "--optimize-interval",  optint, "--output-state", outputstate,  "--output-topic-keys", outputtopickeys, "--output-doc-topics", outputdoctopics, sep = " ")
 
-# setup system enviroment for R
-MALLET_HOME <- "~/mallet" # location of the bin directory
-# Sys.setenv("MALLET_HOME" = MALLET_HOME)
-# Sys.setenv(PATH = "c:/Program Files (x86)/Java/jre7/bin")
 
-# send commands to the Windows command prompt
+# send commands to the Terminal command prompt
 # watch results scroll by in R console...
-#shell(shQuote(paste(cd, import, train, sep = " && ")), 
-#         invisible = FALSE)
+system(paste(cd, "ls", sep=" ; "))
+system(import)
+system(train)			# for consorts (1,754 documents ) and 10 topics, took 2h 46m 36s
 
-system(paste(cd, import, train, sep = " ; "), invisible = FALSE)
  
 # inspect results
 setwd(MALLET_HOME)
 # outputstateresult <-  
 outputtopickeysresult <- read.table(outputtopickeys, header=F, sep="\t")
-outputdoctopicsresult <-read.table(outputdoctopics, header=F, sep="\t")
+outputdoctopicsresult <- read.table(outputdoctopics, header=F, sep="\t")
 
 # manipulate outputdoctopicsresult to be more useful 
 dat <- outputdoctopicsresult
