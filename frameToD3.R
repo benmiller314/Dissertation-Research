d733d43d6d38d0386f34141d35b2ee069b60d652
benmiller314 <benmiller314@gmail.com>
2014-09-27 22:54:44 -0400

    save data table to file for inspection in Excel

--- frameToD3.R
+++ frameToD3.R
@@ -11,7 +11,7 @@
 
 # Ben: set params
 dataset_name <- "consorts"
-ntopics <- 150
+ntopics <- 55
 cutoff <- 150	
 
 
@@ -46,6 +46,16 @@ frameToJSON <- function(dt="outputfile.dt", groupVars, dataVars, outfile) {
   
   #take a look at your strucutre:
   plot(hc)
+  rect.hclust(hc, k=50)
+
+	
+  rect.hclust(hc, h=1.5, border="blue")
+  abline(1.5, 0)
+  rect.hclust(hc, k=10, border="#99FF99")
+  rect.hclust(hc, k=50, border="#FF9999")
+  x <- identify(hc, n=13)
+  rect.hclust(hc, k=55, border="#9999FF")
+
   
   #now we split the data based on membership structure. We will take four levels:
   #(basically this means we will calculate which group each variable belongs in for different levels of the tree strucutre)
@@ -53,12 +63,15 @@ frameToJSON <- function(dt="outputfile.dt", groupVars, dataVars, outfile) {
   # then cut the tree to find group assignments above/below those splits. NB cutree() also allows us to 
   # split the tree at specific heights (on the y axis of that plot), if we don't want to count the groups.
   memb2 <- as.character(cutree(hc, k = 2))
-  memb6 <- as.character(cutree(hc, k = 6))
-  memb15 <- as.character(cutree(hc, k = 15))
-  memb40 <- as.character(cutree(hc, k = 40))
+  memb5 <- as.character(cutree(hc, k = 5))
+  memb10 <- as.character(cutree(hc, k = 10))
+  memb22 <- as.character(cutree(hc, k = 22))
+  # memb55 <- as.character(cutree(hc, k = 55))
   
   # Ben: list these out so we can distinguish them from node and edge variables
-  membVars <- c("memb2", "memb5", "memb10", "memb22", "memb55")
+  membVars <- c("memb2", "memb5", "memb10", "memb22"
+   # , "memb55"
+   )
 
   # get topic labels, which you've composed elsewhere using 'top docs per topic.R'
   filename <- paste0(imageloc, "topic labeling - ", dataset_name, ", K", ntopics, ".csv")
@@ -75,12 +88,15 @@ frameToJSON <- function(dt="outputfile.dt", groupVars, dataVars, outfile) {
 
 
   #Now put this information into a table, together with the labels and the order in which they should appear:
-  b <- data.table(memb2,memb5,memb10,memb22, memb55,label=topic.labels.dt[, Label],order=hc$order)
+  b <- data.table(memb2,memb5,memb10,memb22
+  # , memb55
+  ,label=topic.labels.dt[, Label], topwords=topic.labels.dt[, Top.Words], order=hc$order)
   
   #We might want to know the size of each node. Let's add that
   # Ben: for a topic model, this will find the total %-point contribution of the topic to all docs;
   # that means we could divide by number of docs to scale to [0,1], but no need: it's proportional.
   b$size <- colSums(outputfile.dt[,c(dataVars),with=F])
+  b$scaledsize <- b$size/nrow(outputfile.dt)
   
   # get co-occurring topics, for hierarchical edge bundling
   if(!exists("get.cotopics")) { source(paste0(sourceloc, "cotopics.R")) }
@@ -102,7 +118,15 @@ frameToJSON <- function(dt="outputfile.dt", groupVars, dataVars, outfile) {
   
   # drop the source variable
   b[,source:=NULL]
- 
+
+  # Save this data table to a csv for later inspection
+  if(remake_figs) {
+  	filename <- paste0(imageloc, "topic clusters - ", dataset_name, ", K", ntopics, ".csv")
+  	write.csv(b, filename)
+  } else {
+  	print(b)
+  }
+  
   #we define a function which will create a nested list in JSON format:
   #From here: http://stackoverflow.com/questions/12818864/how-to-write-to-json-with-children-from-r
   # Ben: but see also, now, http://bit.ly/1jXAC5M
@@ -117,9 +141,9 @@ frameToJSON <- function(dt="outputfile.dt", groupVars, dataVars, outfile) {
     }else{
       lapply(seq(nrow(x[1])), function(y){
       	list(name=x[,1][y],
-      		 size=x[,2][y],
-      		 targets=x[,3][y],
-      		 weights=x[,4][y]
+      		 size=x[,2][y]
+      		 # , targets=x[,3][y],
+      		 # weights=x[,4][y]
       	)})
     }
   }
