037b3ed26aa9dc1e2805ed1ac7881911e2ecb2fa
benmiller314 <benmiller314@gmail.com>
2015-03-26 16:47:19 -0400

    optionally norm by row or print dendrogram
    now available from the wraparound function to pass on to heatmap_ben

--- collocation heatmap.R
+++ collocation heatmap.R
@@ -6,7 +6,12 @@
 #  The total number of times dissertations are tagged X is returned separately.
 
 #  1. Calculate tag collocations, total dissertations per tag, and solo counts per tag.
-sumbytags <- function(dataset_name="noexcludes", tagset_name="tagnames", doplot=T) {
+sumbytags <- function(dataset_name="noexcludes", 
+					tagset_name="tagnames", 
+					doplot=T,
+					normed=F,
+					dendro	 = FALSE		# should we output dendrograms showing method clustering?
+					) {
 	dataset <- get(dataset_name)
 	tagset <- get(tagset_name)
 	
@@ -17,13 +22,21 @@ sumbytags <- function(dataset_name="noexcludes", tagset_name="tagnames", doplot=
 		# select the tag
 		tag <- tagset[i]
 		
+		# debug
+		# if(tag %in% c("Poet", "Prac")) { print(sum.by.tags) }
+		
 		# sum columns where the tag is 0 and where it's 1; 
 		# this produces an array with two rows.
 		tagsum <- aggregate(dataset[, tagset], list(dataset[, tag]), FUN=sum)
 		
-		# save the row in which the tag is "on", i.e. row 2. 
+		# debug
+		# if(tag %in% c("Poet", "Prac")) { print(sum.by.tags) }
+		
+		# Save the row in which the tag is "on", i.e. row 2. 
+		# If no such row exists, fill with zeroes to avoid NA results.
 		# First column is the on/off status, so get rid of it.
-		sum.by.tags <- rbind(sum.by.tags, tagsum[2, 2:ncol(tagsum)])
+		if (nrow(tagsum) == 1) { sum.by.tags <- rbind(sum.by.tags, rep(0, ncol(tagsum)-1)) }
+		else { sum.by.tags <- rbind(sum.by.tags, tagsum[2, 2:ncol(tagsum)]) }
 		
 		# Name the row we've just added by the tag we're currently summarizing.
 		row.names(sum.by.tags)[i] <- tag
@@ -42,32 +55,35 @@ sumbytags <- function(dataset_name="noexcludes", tagset_name="tagnames", doplot=
 	# print(sum.by.tags)
 	# print(total.counts)
 
-	to.return <- list("correlations" = as.matrix(sum.by.tags),
+	to.return <- list("dataset" = dataset_name,
+				 "correlations" = as.matrix(sum.by.tags),
 				 "solo.counts" = solo.counts,
 				 "total.counts" = total.counts)
 
 	if(doplot) {
 		if(!exists("heatmap.ben", mode="function")) {source(file="heatmap_ben.R")}
 		
-		# 2. Basic heatmap
-		if(remake_figs) {
-			filename <- paste0(imageloc, "Method Tag Co-Occurrence, ", dataset_name, ", N", nrow(dataset), ".pdf")
-			pdf(filename) 
-		} 
-			heatmap.ben(to.return, diags=TRUE)
-			title(main="Method Tag Co-Occurrence", sub=paste0(dataset_name, ", N", nrow(dataset)))
-			mtext("A box in row Y, column X gives the number of dissertations tagged Y that are also tagged X", side=4)
-		if(remake_figs) { dev.off() }
-		
-		# 3. Normed heatmap
-		if(remake_figs) { 
-			filename <- paste0(imageloc, "Method Tag Co-Occurrence (normed by row), ", dataset_name, ", N", nrow(dataset), ".pdf")
-			pdf(filename) 
+		if(!normed) {
+			# 2. Basic heatmap
+			if(remake_figs) {
+				filename <- paste0(imageloc, "Method Tag Co-Occurrence, ", dataset_name, ", N", nrow(dataset), ".pdf")
+				pdf(filename) 
+			} 
+				heatmap.ben(to.return, diags=TRUE, dendro=dendro)
+				title(main="Method Tag Co-Occurrence", sub=paste0(dataset_name, ", N", nrow(dataset)))
+				mtext("A box in row Y, column X gives the number of dissertations tagged Y that are also tagged X", side=4)
+			if(remake_figs) { dev.off() }
+		} else {
+			# 3. Normed heatmap
+			if(remake_figs) { 
+				filename <- paste0(imageloc, "Method Tag Co-Occurrence (normed by row), ", dataset_name, ", N", nrow(dataset), ".pdf")
+				pdf(filename) 
+			}
+				heatmap.ben(to.return, rowscale=TRUE, diags=TRUE, dendro=dendro)
+				title(main="Method Tag Co-Occurrence \n (normed by row)", sub=paste0(dataset_name, ", N", nrow(dataset)))
+				mtext("A box in row Y, column X gives the probability that a dissertation tagged Y is also tagged X", side=4)
+			if(remake_figs) { dev.off() }
 		}
-			heatmap.ben(to.return, rowscale=TRUE, diags=TRUE)
-			title(main="Method Tag Co-Occurrence \n (normed by row)", sub=paste0(dataset_name, ", N", nrow(dataset)))
-			mtext("A box in row Y, column X gives the probability that a dissertation tagged Y is also tagged X", side=4)
-		if(remake_figs) { dev.off() }
 	}
 			
 	return (to.return)
