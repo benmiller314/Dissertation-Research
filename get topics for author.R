6b75cac5dd84684347abc48e3a023991891d1705
benmiller314 <benmiller314@gmail.com>
2015-05-20 11:40:01 -0400

    allow for multiple search results
    prompt user to specify by row number

--- get topics for author.R
+++ get topics for author.R
@@ -4,10 +4,23 @@
 if(!exists("get.topics4doc", mode="function")) { source(file="top docs per topic.R") }
 get.topics4author <- function(authorname, dataset_name="consorts", ntopics=55, howmany=10, showlabels=TRUE) {
 
-	pubnum <- noexcludes[grep(authorname, noexcludes$Author), "Pub.number"]
-	get.topics4doc(pubnum, dataset_name, ntopics, howmany, showlabels)	
+	pubnum <- noexcludes[grep(authorname, noexcludes$Author, ignore.case=T), "Pub.number"]
+	if(length(pubnum) == 1) {
+		print(noquote(paste("$author: ", noexcludes.dt[as.character(pubnum), list(Author)]$Author)))
+		print(get.topics4doc(pubnum, dataset_name, ntopics, howmany, showlabels))
+	} else if (length(pubnum) > 1) {
+		message("More than one match; please use exact author name from list below.")
+		results <- noexcludes.dt[as.character(pubnum), list(Author, Title)]
+		print(results)
+		
+		a <- as.integer(readline("Search again using row number... "))
+		pubnum <- results[a, Pub.number]
+		print(noquote(paste("$author: ", results[a, Author])))
+		print(get.topics4doc(pubnum, dataset_name, ntopics, howmany, showlabels))
+	}
 }
 
 if(autorun) {
 	get.topics4author("MUELLER, DEREK")			
+	get.topics4author("Lucas")
 }	
