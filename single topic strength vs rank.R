f09bfe8d4ec98ee5a4729923789f034601f2c52c
benmiller314 <benmiller314@gmail.com>
2015-06-20 20:57:23 -0400

    use X instead of O as plot symbol

--- single topic strength vs rank.R
+++ single topic strength vs rank.R
@@ -70,6 +70,7 @@ strength_v_rank <- function(my.topic,
 		 ylab="Topic Contribution within Document", 		# y-axis label
 		 ylim=c(0, ymax),									# use the same y-axis for better comparison between plots
 		 xlim=c(0, ncol(grid)),								# same for x-axis
+		 pch=4												# use an X instead of a circle
 	)
 	mtext(subtitle)
 	legend("topright", legendtext, bty="n")
