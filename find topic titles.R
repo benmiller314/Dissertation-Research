1e740859bfae37b193288af631f0784ec176b01c
benmiller314 <benmiller314@gmail.com>
2016-06-15 15:10:54 -0400

    parallelize title-finding function

--- find topic titles.R
+++ find topic titles.R
@@ -3,22 +3,39 @@ find_topic_titles <- function(dataset_name = "consorts",
                               ntopics      = 55, 
                               subset_name  = NULL) 
 {
-    titles_all <- data.frame(topic=1:ntopics)
+    # run the loop in parallel, because it's annoyingly slow and each run is independent
+    require(doParallel)
+    require(foreach)
     
-    for (i in 1:ntopics) {
-        titles <- top_topic_browser(for.bind=T, subset_name=subset_name, topic=i)$Title
-        one_topic_titles <- ""
-        for (j in titles) {
-            print(j)
-            one_topic_titles <- paste(one_topic_titles, j, sep="||")
-        }
-        titles_all[i, "titles"] <- one_topic_titles
+    cl <- makeCluster(detectCores()-1)
+    registerDoParallel(cl)
+    
+    titles_all <- foreach (i=1:ntopics, 
+                           .verbose=T,
+                           .export=c("dataset_name", "ntopics", "subset_name", "realconsorts", 
+                                     "get.doc.composition", "get.doctopic.grid", "get.topickeys", 
+                                     "get.topics4doc", "imageloc", "noexcludes.dt", "remake_figs", 
+                                     "subset_name", "tagnames", "tmloc", "top_topic_browser", 
+                                     "unixsourceloc"),
+                           .combine="rbind"
+                           ) %dopar% {
+        titles <- factor(top_topic_browser(for.bind=T, subset_name=subset_name, topic=i)$Title)
+        one_topic_titles <- paste(titles, collapse=" || ")
+        cbind(topic=i, top_titles=one_topic_titles)
     }
+    
+    stopCluster(cl)
  
     if(remake_figs) {
-        filename <- paste0(unixsourceloc, "top_titles_per_topic-", dataset_name, "k", ntopics, subset_name, ".csv")
+        filename <- paste0("top_titles_per_topic-", dataset_name, "k", ntopics, subset_name, ".csv")
         write.csv(titles_all, paste0(imageloc, filename))
     } else {
         return(titles_all)
     }
 }
+
+if(autorun) {
+    remake_figs
+    find_topic_titles(subset_name = "realconsorts")    
+}
+
