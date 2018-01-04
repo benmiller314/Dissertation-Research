73fc546ee3a956d5c180ec41babef070cdc22424
benmiller314 <benmiller314@gmail.com>
2018-01-03 23:44:18 -0500

    major changes to deal with changed MALLET output
    apparently the composition.txt file no longer sorts topics in ranked order, alternating between topic number and weight. which is, ultimately, good, but couldn't they have indicated that somewhere??

--- get doctopic grid.R
+++ get doctopic grid.R
@@ -1,4 +1,4 @@
-#############################################################################
+d#############################################################################
 # get doctopic grid.R
 # 
 # GOAL: Read in a table of documents with all topic proportions for each.
@@ -19,57 +19,91 @@ get.doctopic.grid <- function(dataset_name="consorts",
     # get packages in case we've just restarted R
     require(data.table)
     
+    
+    ################
+    ## OH GOOD LORD -- encountering mysterious bugs on 2018-01-02, I realize that MALLET's output has changed:
+    ## instead of spitting out alternating ranked columns of topic number, topic weight, topic number, topic weight 
+    ## as my original reshaping scheme below assumed), at some point it started giving only topic weights, with all topics 
+    ## in order. This change was not documented anywhere, as far as I can tell, except for this issue queue -- from way back
+    ## in 2015 -- https://github.com/mimno/Mallet/issues/41. GRRRRRAGHH.
+    ##
+    ## The upshot is, we no longer need mallet_composition2r.py, because the composition file is already in a grid. 
+    ################
+    
+    #
     # Locate the doc/topic grid, or create it if it doesn't yet exist.
-    filename <- file.path(tmloc, paste0(dataset_name, "k", ntopics, 
-                        "_doc-all-topics_", iter_index, ".txt"))
+    # filename <- file.path(tmloc, paste0(dataset_name, "k", ntopics, 
+    #                     "_doc-all-topics_", iter_index, ".txt"))
+    # 
+    # if (!file.exists(filename)) {   
+    #     if (do_reshape) { cmd <- "reshapeMallet.py" } else { cmd <- "mallet_composition2r.py" }
+    #     command <- paste0("cd '", unixsourceloc, 
+    #             "'; cd 'Shell scripts and commands'; python", cmd)
+    #     
+    #     go <- readline(paste("Have you updated", cmd,
+    #                     "to reflect your current dataset/ntopics? (Y/N)\n"))
+    #     if(tolower(go) != "y") { 
+    #         stop("Better fix that, then") 
+    #     } 
+    #     
+    #     print("Converting topic/weight pairs into doc/topic grid...")
+    #     if(! system(command)) {     # runs only if command exits successfully
+    #         print("Done.") 
+    #     }
+    #     
+    # } else {    # file already exists
+    #     print("Oh, good, the doc-all-topics file exists. Moving on...")
+    # }
     
-    if (!file.exists(filename)) {   
-        if (do_reshape) { cmd <- "reshapeMallet.py" } else { cmd <- "mallet_composition2r.py" }
-        command <- paste0("cd '", unixsourceloc, 
-                "'; cd 'Shell scripts and commands'; python", cmd)
-        
-        go <- readline(paste("Have you updated", cmd,
-                        "to reflect your current dataset/ntopics? (Y/N)\n"))
-        if(tolower(go) != "y") { 
-            stop("Better fix that, then") 
-        } 
-        
-        print("Converting topic/weight pairs into doc/topic grid...")
-        if(! system(command)) {     # runs only if command exits successfully
-            print("Done.") 
-        }
-        
-    } else {    # file already exists
-        print("Oh, good, the doc-all-topics file exists. Moving on...")
+    # Read in the doc/topic grid. 
+    filename <- file.path(tmloc, paste0(dataset_name, "k", ntopics, "_composition_", iter_index, ".txt"))
+    doc_topics <- read.delim(filename, 
+                             header=FALSE, 
+                             colClasses=c("NULL",                   # first col is rownumber; skip
+                                          "character",              # second col is filename
+                                          rep("numeric", ntopics)   # all other cols are topic weights
+                                          )
+                             )
+    
+    
+    # column 1 is an unneeded index; column 2 contains filenames starting with "clean_", 
+    # ending with a 7-digit Pub.number followed by ".txt". Let's simplify.
+    
+    get_pub <- function(cell) {
+        cell <- as.character(cell)
+        last_underscore <- tail(gregexpr("_", cell)[[1]], 1)    
+        val <- substr(cell, last_underscore + 1, nchar(cell)-4)
+        return(val)
     }
+    doc_topics[, "V2"] <- sapply(doc_topics[, "V2"], get_pub)
     
-    # Read in the doc/topic grid
-    outputfile <- read.delim(filename, header=F)
     
-    # switch from 0-indexed to 1-indexed, so the topic numbers in
-    # topic_keys.dt are the same as row numbers
-    # NB: this seems to be necessary to avoid searching for column "0"
-    head(outputfile,3)
-    names(outputfile) <- c("Pub.number", (1:(ncol(outputfile)-1)))
-    head(outputfile)
- 
+    # first remaining column is now the text id; call it Pub.number to match main data structure.
+    # remaining columns are topics in order. To avoid searching for column "0" later,
+    # switch from 0-indexed to 1-indexed, so the topic numbers in topic_keys.dt are the same as row numbers
+
+    colnames(doc_topics) <- c("Pub.number", (1:(ncol(doc_topics)-1)))
+    head(doc_topics,3)
+     
+                                 
     # If we accidentally included .DS_Store in the mallet input, remove it now
-    if(any(outputfile["Pub.number"] == "Store")) {
+    if(any(doc_topics["Pub.number"] == "S")) {
         # nrow(outputfile)
-        replacement <- outputfile[-which(outputfile["Pub.number"] == "Store"),]
+        replacement <- doc_topics[-which(doc_topics["Pub.number"] == "S"),]
         # head(replacement, 3)
         # nrow(replacement)
-        outputfile <- replacement
-        str(outputfile)
+        doc_topics <- replacement
+        str(doc_topics)
         rm(replacement)
     }
     
+    head(doc_topics,3)
     
     
     # If we specified a subset, filter the data tables before moving on
     if (! is.null(subset_name)) {
         subset_index <- as.integer(get(subset_name)$Pub.number)
-        outputfile <- outputfile[subset_index,]
+        doc_topics <- doc_topics[subset_index,]
     }
     
     
@@ -78,10 +112,10 @@ get.doctopic.grid <- function(dataset_name="consorts",
     # Each cell gives the percentage which the topic in that column
     # and sorting gives us a rank based on percentage points.
     
-    colsums <- colSums(outputfile[,which(names(outputfile) != "Pub.number")])
+    colsums <- colSums(doc_topics[,which(names(doc_topics) != "Pub.number")])
     str(colsums)
     colsums <- c(999999, colsums)
-    names(colsums) <- names(outputfile)
+    names(colsums) <- names(doc_topics)
     head(colsums)
     colsums.sort <- colsums[order(colsums, decreasing=TRUE)]
     head(colsums.sort)
@@ -89,7 +123,7 @@ get.doctopic.grid <- function(dataset_name="consorts",
     # Divide the percentage point totals by the number of dissertations
     # to get an overall percent contribution
     # colsums.pct <- round((colsums / nrow(outputfile)), 4) * 100
-    colsums.sort.pct <- round((colsums.sort / nrow(outputfile)), 4) * 100
+    colsums.sort.pct <- round((colsums.sort / nrow(doc_topics)), 4) * 100
     head(colsums.sort.pct)
     
     # Optionally save to file
@@ -133,7 +167,7 @@ get.doctopic.grid <- function(dataset_name="consorts",
     list("colsums" = colsums,
          "colsums.sort" = colsums.sort,
          "colsums.sort.pct" = colsums.sort.pct,
-         "outputfile" = outputfile
+         "outputfile" = doc_topics
          )
 
 }   # end of get.doctopic.grid()
