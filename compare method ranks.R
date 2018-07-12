35a2bba758c473d3dfe2a9d8e1e18073b890b53a
benmiller314 <benmiller314@gmail.com>
2018-07-12 14:37:12 -0400

    add support for other tagsets
    e.g. tagnames.simple

--- compare method ranks.R
+++ compare method ranks.R
@@ -13,12 +13,13 @@ compare_method_ranks <- function(set1="consorts",
 		pcts=TRUE, 			# Label with percent of docs with that tag? 
 							# If FALSE, use real counts. 
 		colorful=FALSE,		# Use multiple colors to distinguish lines?
-		betterlabels=NULL) 	# Optional vector of length 2, giving set labels.
+		betterlabels=NULL, 	# Optional vector of length 2, giving set labels.
+		tagset_name="tagnames")  # Which tags to use?
 {
 		
 	if(!exists("get_tags", mode="function")) { source(file="get tags.R") }
-	b <- get_tags(set1)
-	d <- get_tags(set2)
+	b <- get_tags(set1, tagset_name)
+	d <- get_tags(set2, tagset_name)
 	
 	# Line up tag names 
 	# set1 first:
@@ -39,14 +40,14 @@ compare_method_ranks <- function(set1="consorts",
 					 nrow(get(set2)), 0), "%)")
 	
 		filename <- paste0(imageloc, "Ranks of methods in ", set1, " v ", 
-							set2, ", no Othr, pcts.pdf")
+							set2, "--", tagset_name,", no Othr, pcts.pdf")
 	} else {
 		# Add diss counts to each tag
 		b2 <- paste0(b1, " (", b0[order(b0, decreasing=T)], ")")	
 		d2 <- paste0(d1, " (", d0[order(d0, decreasing=T)], ")")
 	
 		filename <- paste0(imageloc, "Ranks of methods in ", set1, " v ", 
-							set2, ", no Othr.pdf")
+		                   set2, "--", tagset_name,", no Othr, counts.pdf")
 	}
 	
 	## Test significance of any differences
@@ -75,16 +76,16 @@ compare_method_ranks <- function(set1="consorts",
 		# by the number of comparisons in the set
 		
 		if(fish$p.value < 0.001 / length(b)) {
-			message(paste(realtags(tag), "is very significantly different 
-			   (Bonferroni corrected p < 0.001) between", set1, "and", set2))
+			message(paste(realtags(tag, tagset_name), "is very significantly different", 
+			   "(Bonferroni corrected p < 0.001) between", set1, "and", set2))
 			return(" ** ")
 		} else if(fish$p.value < 0.05 / length(b)) {
-			message(paste(realtags(tag), "is significantly different 
-			   (Bonferroni corrected p < 0.05) between", set1, "and", set2))
+			message(paste(realtags(tag, tagset_name), "is significantly different", 
+			   "(Bonferroni corrected p < 0.05) between", set1, "and", set2))
 			return("  * ")
 		} else {
-			message(paste(realtags(tag), "is not significantly different 
-			   between", set1, "and", set2))
+			message(paste(realtags(tag, tagset_name), "is not significantly different", 
+			   "between", set1, "and", set2))
 			return("    ")
 		}
 	}
@@ -161,8 +162,8 @@ compare_method_ranks <- function(set1="consorts",
 					 y=rep(length(b)+1,2)
 				)
 			} else {
-				warning("Incorrect number of betterlabels: 
-						must be vector of length 2. Using set names.")
+				warning("Incorrect number of betterlabels: ",
+						"must be vector of length 2. Using set names.")
 				text(labels=c(set1, set2), 
 					 x=c(4,length(b)-4), 
 					 y=rep(length(b)+1,2)
@@ -183,12 +184,16 @@ compare_method_ranks <- function(set1="consorts",
 		)
 		
 		# add legend for significance
-		if(any(grep("*", sig.b))) {
-			mtext("* Bonferroni corrected p < 0.05 \n 
-				   ** Bonferroni corrected p < 0.001",
+		if(any(grep("*", sig.b, fixed=T))) {
+			mtext(paste("* Bonferroni corrected Fisher p < 0.05 \n", 
+				   "** Bonferroni corrected Fisher p < 0.001"),
 				  cex=0.8,
 				  side=2
 			)
+		} else {
+		    mtext("No comparisons significant via Bonferroni corrected Fisher exact test at p < 0.05",
+		          cex=0.8,
+		          side=1)
 		}
 		
 	if (remake_figs) { dev.off() }
@@ -201,6 +206,16 @@ if(autorun) {
 						betterlabels=c("Consortium", "All Non-Consortium"))
 	compare_method_ranks("consorts", "top.nonconsorts", 
 						betterlabels=c("Consortium", "Top Non-Consortium"))
+	compare_method_ranks("noexcludes2001_2005", "noexcludes2006_2010", #tagset_name="tagnames.simple",
+	                     betterlabels=c("All departments, 2001-2005",
+	                                    "All departments, 2006-2010"))
+	compare_method_ranks("noexcludes2006_2010", "noexcludes2011_2015", #tagset_name="tagnames.simple",
+	                     betterlabels=c("All departments, 2006-2010",
+	                                    "All departments, 2011-2015"))
+	compare_method_ranks("realconsorts2001_2005", "realconsorts2006_2010", tagset_name="tagnames.simple",
+	                     betterlabels=c("Consortium programs, 2001-2005",
+	                                    "Consortium programs, 2006-2010"))
+	
 } else {
     message("The following function has been loaded:")
     message("    compare_method_ranks(set1, set2, pcts, colorful, betterlabels, tagset_name)")
