1559d5a34e204ea790d8d8917de60f9c0a260e94
benmiller314 <benmiller314@gmail.com>
2018-07-12 14:26:18 -0400

    count words in text columns, plot against year

---
+++ word_counts.R
@@ -0,0 +1,88 @@
+# count words in a given column (abstracts, eg.) of a data.frame
+
+wc <- function(f) {
+    if(!typeof(f) == "character") {
+        f <- as.character(f)
+    }
+    
+    # split by spaces, count how many items that produces
+    count <- length(unlist(strsplit(f, " ")))
+    
+    return(count)
+}
+
+wc_column <- function(dataset, column, do.plot=T, print.summary=T) {
+    col <- dataset[[column]]
+    
+    counts <- sapply(col, wc)
+    if (do.plot) { 
+        boxplot(counts) 
+    }
+    if(print.summary) {
+        print(summary(counts))    
+    }
+    
+    return(counts)
+}
+
+# for dissertations with knownprograms (as of 2018-07-12), n 1359 years 1993-2015
+# > wc_column(knownprograms, "Title")
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 2.0    10.0    12.0    12.5    15.0    37.0 
+# 
+# > wc_column(knownprograms, "ABSTRACT")
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 58.0   240.0   312.0   294.2   347.0   670.0 
+
+# Does it change over time?
+wc_timeplot <- function(dataset, column, rawavg=F, smoothavg=T) {
+    my_title <- paste("Average word count of", column, "in", deparse(substitute(dataset)))
+    
+    if (remake_figs) {
+        filename <- file.path(imageloc, paste0(my_title, ".pdf"))
+        pdf(filename)
+    }
+    
+    plot.new()
+    plot(dataset[["Year"]], wc_column(dataset, column))
+    years <- sort(unique(dataset[["Year"]]))
+    yearlyavgs <- c()
+    
+    for (my_year in years) {
+        yearlyavg <- mean(wc_column(dataset[which(knownprograms$Year == my_year),], column, do.plot=F, print.summary=F))
+        yearlyavgs <- c(yearlyavgs, yearlyavg)
+    }
+    
+    legend_fill <- legend_text <- c()
+    
+    if(rawavg) {
+        lines(years, yearlyavgs, col="#3300cc")
+        legend_fill <- "#3300cc"
+        legend_text <- "yearly average"
+    }
+    
+    if(smoothavg) {
+        lines(years, runmed(yearlyavgs, 3, "median"), col="#cc0033")
+        legend_fill <- c(legend_fill, "#cc0033")
+        legend_text <- c(legend_text, "average (smoothed by running median, bin=3)")
+    }
+    
+    if (rawavg | smoothavg) {
+        legend("topleft", fill=legend_fill, legend=legend_text)
+    }
+    
+    title(my_title)
+    
+    if(remake_figs) {
+        dev.off()
+    }
+
+       
+}
+
+if(autorun) {
+    wc_column(knownprograms, "ABSTRACT")
+    wc_timeplot(knownprograms, "ABSTRACT")
+    wc_column(knownprograms, "Title")
+    wc_timeplot(knownprograms, "Title")
+}
