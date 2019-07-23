af7408d404eae9ea4a583e21809fcf23516c69a9
benmiller314 <benmiller314@gmail.com>
2019-07-22 22:19:35 -0400

    help position figure legends
    i.e. without clobbering the image itself. should load with R profile.

---
+++ outside_legend.R
@@ -0,0 +1,9 @@
+# from https://stackoverflow.com/questions/3932038/plot-a-legend-outside-of-the-plotting-area-in-base-graphics,
+# a way to add a legend outside of the plot area
+outside_legend <- function(...) {
+    opar <- par(fig=c(0, 1, 0, 1), oma=c(0, 0, 0, 0), 
+                mar=c(0, 0, 0, 0), new=TRUE)
+    on.exit(par(opar))
+    plot(0, 0, type='n', bty='n', xaxt='n', yaxt='n')
+    legend(...)
+}
