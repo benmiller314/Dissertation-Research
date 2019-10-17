4d8de13d9dfb2521b0d24c42fe18dfc7a9353712
benmiller314 <benmiller314@gmail.com>
2019-10-17 16:58:04 -0400

    start removing deprecated files

--- start here.R
+++
@@ -1,4 +0,0 @@
-# Make sure the data exists, please
-source("/Users/benmiller314/Dropbox/coursework, etc/dissertation/data, code, and figures/Dissertation Research/dataprep.R")
-setwd(sourceloc)
-source(file="dataprep 2 - load data.R")
