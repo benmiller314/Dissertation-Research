49205b0cae25f1606a64b3d7202f042087f656c5
benmiller314 <benmiller314@gmail.com>
2020-02-17 11:17:41 -0500

    use data.table when possible (but fail gracefully if not)

--- get tags.R
+++ get tags.R
@@ -5,7 +5,11 @@ get_tags <- function(dataset_name="noexcludes", tagset_name="tagnames") {
 	dataset <- get(dataset_name)
 	tagset <- get(tagset_name)
 	
-	a1 <- dataset[, tagset]
+	if("data.table" %in% class(dataset)) {
+	    a1 <- dataset[, ..tagset]
+	} else {
+	    a1 <- dataset[, tagset]
+	}
 	a2 <- apply(a1, 2, sum)
 	message("Method tag frequency for ", dataset_name, ":")
 	print(a2)
