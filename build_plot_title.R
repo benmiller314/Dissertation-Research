e16818d5921a5d0f58780c6cca762fbedc64a736
benmiller314 <benmiller314@gmail.com>
2020-03-10 16:50:44 -0400

    add parameter for generating filename slugs
    with for.filename=TRUE (default is FALSE), removes spaces. you'll still have to add the extension and the appropriate file.path to the returned value.

--- build_plot_title.R
+++ build_plot_title.R
@@ -1,22 +1,29 @@
 # Ben: helper function to build a filename / main title for topic-modeling figures
-build_plot_title <- function(dataset_name, ntopics, iter_index, subset_name, 
+build_plot_title <- function(dataset_name, ntopics, iter_index, subset_name,
                              bad.topics, use.labels=FALSE,
-                             whatitis="Cluster Dendrogram") {
-    if(! is.null(subset_name)) { subset_part <- paste0("--", subset_name) 
+                             whatitis="Cluster Dendrogram",
+                             for.filename=FALSE) {
+    if(! is.null(subset_name)) { subset_part <- paste0("--", subset_name)
     } else { subset_part <- "" }
-    
+
     if(length(bad.topics > 0)) { bad_topics_part <- paste0(", ", length(bad.topics), " bad topics hidden")
     } else { bad_topics_part <- "" }
-    
-    if(iter_index > 0) { iter_part <- paste0("_iter", iter_index) 
+
+    if(iter_index > 0) { iter_part <- paste0("_iter", iter_index)
     } else { iter_part <- "" }
-    
-    if(use.labels) { label_part <- ", topics labeled" 
+
+    if(use.labels) { label_part <- ", topics labeled"
     } else { label_part <- "" }
-    
+
     main <- paste0(whatitis, ", ",                                       # type of plot/file
                    dataset_name, "k", ntopics, iter_part,                # which topic model
                    subset_part, bad_topics_part,                         # parts of that model
                    label_part)                                           # topics named or numbered
+
+    if(for.filename) {
+        main <- gsub(", ", "--", main)
+  	    main <- gsub(" ", "_", main)
+    }
+    
     return(main)
 }
