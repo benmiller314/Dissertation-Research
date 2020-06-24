86ae186e25c64488f5f685021a08fbdbe9f701e7
benmiller314 <benmiller314@gmail.com>
2020-06-24 11:56:01 -0400

    new function: topic.reach
    Ranks topics by document-reach over all (individual) non-bad topics, at a specified range of reach-levels. Returns a table.

--- topic cluster reach.R
+++ topic cluster reach.R
@@ -44,30 +44,44 @@ name_topic_clusters <- function(dataset_name="noexcludes2001_2015",
         WPA <- c(45, 6, 25, 8)
     }
 
-    if(dataset_name=="noexcludes2001_2015" && ntopics==50 && iter_index==1 && subset_name=="realconsorts2001_2015") {
+    if(dataset_name=="noexcludes2001_2015" && ntopics==50 && iter_index==1 && !is.null(subset_name) && subset_name=="realconsorts2001_2015") {
         Teaching.of.Writing <- c(18, 37, 6, 42, 16, 26, 41, 49, 28, 32, 35)
 
     }
 
 
-    if(dataset_name=="noexcludes2001_2015" && ntopics==50 && iter_index==1 && subset_name=="knownprograms2001_2015") {
+    if(dataset_name=="noexcludes2001_2015" && ntopics==50 && iter_index==1 && !is.null(subset_name) && subset_name=="knownprograms2001_2015") {
 
         # stable clusters under both diana and agnes:
-        big.teaching <- c(1, 6, 11, 18, 27, 35, 37, 40, 41, 42, 44, 45, 49) # includes online / digital / tech-comm topics
+        
         small.teaching <- c(18, 35, 42, 49) # limits it to writing process (18), K-12 writing pedagogy (42),
                                             # scenes of teaching (49), and institutional supports (35)
                                             # (last to join is topic 35; could cut)
-
-        digital.media <- c(44, 45) # digital media and online circulation minicluster
+        digital.media <- c(44, 45)          # digital media and online circulation minicluster
+        big.teaching <- c(1, 6, 11, 18, 27, # includes online / digital / tech-comm topics. a superset
+                          35, 37, 40, 41,   # of small.teaching and digital.media clusters.
+                          42, 44, 45, 49) 
         politics.law <- c(9, 13, 19, 29, 31) # politics and law cluster:
                                              # 9 Historical Public Rhetoric, esp of Women; 13 Politics, Empire, Radicalism;
                                              # 19 Presidential Discourse; 29 Writing in Law and Government;
                                              # 31 Labor Unions and Economic Activism
-
-        ethnicities <- c(5, 17, 23) # race, ethnicity, nationality cluster
-        classics <- c(14, 25, 39)   # poetics/homiletics/classics cluster
-
+        
+        ethnicities <- c(5, 17, 23)          # race, ethnicity, nationality cluster
+        justice <- c(4, 20, 5, 17, 23, 10)   # superset of the ethnicities cluster;
+                                             # leaves out 38 (reading through feminisms) b/c it
+                                             # clusters with rhetoric under diana
+        classics <- c(14, 25, 39)            # poetics/homiletics/classics cluster
+        
         reading.rhetoric <- c(33, 48) # 33 Rhetorical Frameworks and 48 Reading Rhetoricians, Interpreting Philosophy
+        bodies.and.power <- c(2, 21)  # microcluster of 2 Image, Body, Mind and 21 Rhetorics of Power, Conflict, Politics
+        disciplinary.literacies <- c(28, 32) # microcluster of 28 Literacy Practices and 32 Disciplinary Formations
+        big.rhetoric <- c(2, 21, 48, 33, 28, 32)  # superset of previous three clusters
+        
+        narrative <- c(16, 26)               # 16 creative writing/reflection 26 personal narr. + oral history
+        entertainment <- c(7, 43)            # 7 popular media (film, tv, music) + 43 sports
+        
+        all_clusters <- c("small.teaching", "politics.law", "justice", "classics",
+                          "big.rhetoric", "narrative")
     }
 
     # return variables created by this function
@@ -100,6 +114,7 @@ cluster.strength <- function (my.topics=NULL,      # either pass in a list of to
                             use.labels = FALSE   # set TRUE if you want to pass labels
 ){
     # Which topics are in the cluster?
+    # to do: combine my.topics and my.topics_name and more gracefully detect which is which
     if(is.null(my.topics)) {
         if(is.null(my.topics_name)) {
             stop("cluster.strength() requires you to specify a cluster by passing either my.topics or my.topics_name.")
@@ -221,6 +236,106 @@ cluster.strength <- function (my.topics=NULL,      # either pass in a list of to
                     "topics" = my.topics))
 }
 
+
+## Get ranks by topic reach over all (individual) non-bad topics
+#  (Find extent_level from mystats using find.doc.by.topic.proportion() in `variation of topic proportions.R`;
+#   mystats$Lhinge[1:5] means the 25th percentile for the top five topic-ranks)
+topic.reach <- function(my.topic = NULL, # one or several numbers. if NULL, uses 1:ntopics.
+                        dataset_name = "noexcludes2001_2015",
+                        ntopics = 50,
+                        iter_index = 1,
+                        subset_name = "knownprograms2001_2015",
+                        extent_level = c(0.05, 0.06, 0.09, 0.13, 0.22, 0.33, 0.4, 0.5, 0.66, 0.75),
+                        use.labels = T,
+                        labels = NULL,  # pass topic_labels if it exists    
+                        grid = NULL,    # pass doc-topic matrix (usually called dt) if it exists
+                        newnames = F)   # only used if grid is NULL
+{
+    require(data.table)
+    topic_reach <- data.table()
+    
+    # make sure we have what we need
+    if (is.null(extent_level)) {
+        tryCatch(extent_level <- sort(round(mystats$Lhinge[1:5], 2)),
+                 e = stop("get mystats from find.doc.by.topic.proportion()"),
+                 finally = ""
+        )
+    }
+    
+    if (use.labels && is.null(labels)) {
+        source(file = "get topic labels.R")
+        topic_labels <- get_topic_labels(dataset_name = dataset_name,
+                                         ntopics = ntopics,
+                                         iter_index = iter_index,
+                                         subset_name = subset_name,
+                                         bad.topics = bad.topics,
+                                         newnames = newnames)
+    }
+    
+    if (is.null(grid)) {
+        source(file = "get doctopic grid.R")
+        grid <- get.doctopic.grid(dataset_name = dataset_name,
+                                  ntopics = ntopics,
+                                  iter_index = iter_index,
+                                  subset_name= subset_name,
+                                  newnames = newnames)
+    }
+    
+    # okay, so what are we looking at? 
+    if (is.null(my.topic)) {           # all topics
+        my.topics <- setdiff(1:ntopics, bad.topics)
+    } else {                           # one or more specified topics
+        my.topics <- my.topic
+    }
+    
+    # output holder
+    topic_reach <- data.table()
+    
+    # main loop
+    for (lvl in extent_level) {
+        extent_all <- sapply(my.topics, FUN=function(x) {
+            cluster.strength(my.topics = x,
+                             dataset_name = dataset_name,
+                             ntopics = ntopics,
+                             iter_index = iter_index,
+                             subset_name = subset_name,
+                             bad.topics = bad.topics,
+                             cumulative = FALSE,
+                             level = lvl,
+                             grid = dt,
+                             use.labels = T,
+                             labels = topic_labels)     # from get_topic_labels()
+        })
+        
+        extent_all.vector <- round(100*unlist(extent_all["percentage", ]), 2)
+        extent_all["grid", ]
+        colname <- quote(paste0("extent_all", lvl*100))
+        topic_reach[, eval(colname)] <- extent_all.vector    
+        topic_reach$label <- unlist(extent_all["name", ])
+        topic_reach$topic <- unlist(extent_all["topics", ])
+    }
+    
+    topic_reach <- topic_reach[order(topic_reach[, 1], decreasing = T)]
+    setcolorder(topic_reach, c("topic", "label"))
+    
+    if(remake_figs) {
+        whatitis <- trimws(paste("Topic Reach", paste(my.topics, collapse="-")))
+        outfile_slug <- build_plot_title(dataset_name = dataset_name,
+                                         ntopics = ntopics,
+                                         iter_index = iter_index,
+                                         subset_name = subset_name,
+                                         bad.topics = bad.topics,
+                                         whatitis = whatitis,
+                                         for.filename = TRUE)
+        outfile <- file.path(imageloc, paste0(outfile_slug, ".csv"))
+        write.csv(topic_reach, outfile, row.names = F)
+    }
+    
+    return(topic_reach)
+}
+
+
+
 # testing zone
 if(FALSE) {
     # cluster.strength("Teaching.of.Writing")
@@ -254,8 +369,8 @@ if(FALSE) {
                      subset_pubs = realconsorts2001_2015$Pub.number,
                      cumulative=F)
 
-    my.topics <- c(6, 11, 18, 42, 49, 35, 41, 37, 27) # smaller diana teaching cluster
-
+    # my.topics <- c(6, 11, 18, 42, 49, 35, 41, 37, 27) # smaller diana teaching cluster
+    # my.topics <- c(4, 20, 5, 17, 23, 10) # social justice / identities cluster
 
 
     cluster_list <- name_topic_clusters(dataset_name = dataset_name,
@@ -337,51 +452,13 @@ if(FALSE) {
     # cluster strength, and a dataseries for each cluster (all on the same
     # graph)
     
-    
-    ## Get ranks by topic reach over all non-bad topics
-    #  (Find mystats with find.doc.by.topic.proportion() in `variation of topic proportions.R`;
-    #   mystats$Lhinge[1:5] means the 25th percentile for the top five topic-ranks)
-    require(data.table)
-    topic_reach <- data.table()
-    extent_level <- sort(round(mystats$Lhinge[1:5], 2))
 
-    for (lvl in extent_level) {
-        extent_all <- sapply(setdiff(1:ntopics, bad.topics), FUN=function(x) {
-            cluster.strength(my.topics = x,
-                         dataset_name = dataset_name,
-                         ntopics = ntopics,
-                         iter_index = iter_index,
-                         subset_name = subset_name,
-                         bad.topics = bad.topics,
-                         cumulative = FALSE,
-                         level = lvl,
-                         grid = dt,
-                         use.labels = T,
-                         labels = topic_labels)     # from get_topic_labels()
-            })
-        
-        extent_all.vector <- round(100*unlist(extent_all["percentage", ]), 2)
-        extent_all["grid", ]
-        colname <- quote(paste0("extent_all", lvl*100))
-        topic_reach[, eval(colname)] <- extent_all.vector    
-        topic_reach$label <- unlist(extent_all["name", ])
-        topic_reach$topic <- unlist(extent_all["topics", ])
-    }
+    cluster.strength(my.topics = c(7, 43),
+                     my.topics_name = "entertainment",
+                     bad.topics = bad.topics,
+                     level = 0.05,
+                     subset_name = subset_name)
     
-    topic_reach <- topic_reach[order(topic_reach[, 1], decreasing = T)]
-    setcolorder(topic_reach, c("topic", "label"))
-    
-    if(remake_figs) {
-        outfile_slug <- build_plot_title(dataset_name = dataset_name,
-                                    ntopics = ntopics,
-                                    iter_index = iter_index,
-                                    subset_name = subset_name,
-                                    bad.topics = bad.topics,
-                                    whatitis = "Topic Reach",
-                                    for.filename = TRUE)
-        outfile <- file.path(imageloc, paste0(outfile_slug, ".csv"))
-        write.csv(topic_reach, outfile, row.names = F)
-    }
     
 
 }
