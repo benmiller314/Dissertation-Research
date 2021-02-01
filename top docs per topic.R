2ad5af178e3f201fe662cfa1c5580287d3608c65
benmiller314 <benmiller314@gmail.com>
2021-02-01 10:59:18 -0500

    better handling of topic keys and labels
    through a new parameter, making the text labels optional

--- top docs per topic.R
+++ top docs per topic.R
@@ -56,6 +56,7 @@ get.topics4doc <- function(pubnum,
                                                # Gets passed into get.doctopic.grid.
                            howmany = 5,
                            showlabels = FALSE,
+                           topic_labels = NULL,
                            columns = c("Title", "Pub.number", tagnames, "realconsort", "realrhetmap"),
                            grid = NULL,         # if it exists, pass in for a speed boost
                            verbose = F)
@@ -64,7 +65,7 @@ get.topics4doc <- function(pubnum,
     require(data.table)
 
     dataset.dt <- data.table(get(dataset_name), key="Pub.number")
-    
+
     # pubnum <- "3051708"; doc_tops <- doc_topics.dt    # test values
 
     if (!is.character(pubnum)) {
@@ -77,48 +78,55 @@ get.topics4doc <- function(pubnum,
                                   iter_index=iter_index, newnames=newnames)$outputfile.dt
     } else {
         doc_tops <- grid
+        doc_tops <- na.omit(doc_tops)
     }
 
     # narrow to the doc named by the pubnum; skip the pubnum for sorting by descending weight
     this_doc_tops <- doc_tops[pubnum, -"Pub.number"]
     this_doc_tops <- this_doc_tops[, order(this_doc_tops, decreasing = T), with=F][,1:howmany, with=F]
 
-    # merge weights with keys: from all keys, narrow to just the top howmany in this doc
-    topic_keys <- data.table(get.topickeys(dataset_name = dataset_name, 
-                                           ntopics = ntopics, 
-                                           iter_index = iter_index, 
-                                           newnames = newnames))
-    topic_keys <- topic_keys[as.numeric(names(this_doc_tops))]
-    topic_keys[,weight:=as.numeric(this_doc_tops)]
-
-    # reorder columns for ease of presentation
-    topic_keys <- topic_keys[, list(topic, weight, alpha, top_words)]
-
     if(showlabels) {
-        if(!exists("get_topic_labels", mode="function")) {
-            source(file="get topic labels.R")
+        if(is.null(topic_labels)) {
+            if(!exists("get_topic_labels", mode="function")) {
+                source(file="get topic labels.R")
+            }
+            topic_labels <- data.table(get_topic_labels(dataset_name = dataset_name,
+                                                        ntopics = ntopics,
+                                                        iter_index = iter_index,
+                                                        subset_name = subset_name),
+                                       key="Topic")
         }
-        topic_labels <- data.table(get_topic_labels(dataset_name = dataset_name, 
-                                                    ntopics = ntopics, 
-                                                    iter_index = iter_index,
-                                                    subset_name = subset_name),
-                                   key="Topic")
-        topic_keys[, current_label:=topic_labels[topic_keys$topic, Label]]
-        topic_keys <- topic_keys[, list(topic, weight, alpha, current_label,
-                                        top_words)]
-        topic_keys
+
+        topic_keys <- topic_labels[as.numeric(names(this_doc_tops))]
+        setnames(topic_keys, old = c("Topic", "Label","Pct.Contrib", "Top.Words"),
+                             new = c("topic", "current_label", "weight", "top_words"))
+
+        # reorder and simplify
+        topic_keys <- topic_keys[, list(topic, weight, current_label, top_words)]
+
+    } else {
+
+        # merge weights with keys: from all keys, narrow to just the top howmany in this doc
+        topic_keys <- data.table(get.topickeys(dataset_name = dataset_name,
+                                               ntopics = ntopics,
+                                               iter_index = iter_index,
+                                               newnames = newnames))
+        topic_keys <- topic_keys[as.numeric(names(this_doc_tops))]
+        topic_keys[,weight:=as.numeric(this_doc_tops)]
+
+        # reorder columns for ease of presentation
+        topic_keys <- topic_keys[, list(topic, weight, alpha, top_words)]
     }
-    
-    
+
     to.return <- list("title" = dataset.dt[pubnum, ..columns],
         "keys" = topic_keys,
         "abstract" = dataset.dt[pubnum, c("KEYWORDS", "ABSTRACT"), with=F]
         )
-    
+
     if(verbose) {
         print(to.return)
     }
-    
+
     return(to.return)
 
 # close helper function get.topics4doc
@@ -170,7 +178,7 @@ top_topic_browser <- function(
         source(file=file.path(sourceloc, "get doctopic grid.R"))
     }
 
-    
+
     grids <- get.doctopic.grid(dataset_name=dataset_name, ntopics=ntopics, subset_name=subset_name, iter_index=iter_index, newnames=newnames)
         colsums <- grids$colsums
         colsums.sort <- grids$colsums.sort
@@ -180,7 +188,7 @@ top_topic_browser <- function(
 
     # get document data
     dataset.dt <- data.table(get(dataset_name), key="Pub.number")
-    
+
     if(! exists("get.topickeys", mode = "function")) {
         source(file=file.path(sourceloc, "get topickeys.R"))
     }
@@ -303,7 +311,7 @@ top_topic_browser <- function(
         # set up a container for docs to return
         to.return <- data.table()
     }
-    
+
     for (i in start.rank:len) {
         # i gives the topic rank
         topic.num <- ind[i]
@@ -323,7 +331,7 @@ top_topic_browser <- function(
         print(topic_keys.dt[as.integer(topic.num)])
 
         # list of top 1:depth documents for this topic
-        topdocs <- dataset.dt[as.character(diss.ind),                
+        topdocs <- dataset.dt[as.character(diss.ind),
                         c("Pub.number", "Title", tagnames), with=F]
 
             # add a column with the weights this topic has in these docs
@@ -345,24 +353,24 @@ top_topic_browser <- function(
             topdocs <- topdocs[, c("Pub.number", "Title", "topic_weight",
                                  "rank_in_doc", tagnames), with=F]
 
-        
-        
+
+
         if(for.bind) {
             # presumably we'll want to distinguish one topic from another
             topdocs[, topic:=topic.num]
             topdocs[, topic_rank:=i]
             setcolorder(topdocs, c("topic", "topic_rank"))
-            
-            # but still have the option to return multiple topics 
+
+            # but still have the option to return multiple topics
             to.return <- rbind(to.return, topdocs)
-            
+
             if(i > cutoff) {
                 stop("Well, this is embarrassing. Somehow we got above the cutoff. Please
                         debug(top_topic_browser).")
-            
+
             } else if (i == cutoff) {
                 return(to.return)
-                
+
             } else {
                 # continue to next topic in loop
                 a <- "s"
@@ -370,14 +378,14 @@ top_topic_browser <- function(
         } else if (remake_figs) {
             # if outputting to file, get all the details
             print(topdocs)
-            
+
             a <- ""
-            
+
         } else {
-            # if neither binding to a variable or outputting to file, 
+            # if neither binding to a variable or outputting to file,
             # assume we're in interactive mode and prompt for what to do next.
             print(topdocs)
-            
+
             a <- readline(paste("Press <enter> for more detail",
                                 "on these docs, or S to skip to the next topic: \n"))
         }
@@ -503,7 +511,7 @@ shareable_topic <- function(  # where will our topic assignments come from?
     ignore.cols <- c(tagnames, tagnames.simple, "Method.Terms", "Flag.notes", "Method.Count", "Exclude.Level", "Counts.simple")
 
     dataset.dt <- data.table(get(dataset_name), key="Pub.number")
-    
+
     docs <- dataset.dt[index]
     docs <- docs[, !(names(dataset.dt) %in% ignore.cols), with=F]
     # names(docs)
@@ -529,14 +537,14 @@ shareable_topic <- function(  # where will our topic assignments come from?
                                      subset_name=subset_name,
                                      whatitis=paste("top", depth, "documents for topic", topic),
                                      for.filename = T)
-        
+
         write.csv(docs, filename)
     } else {
         warning("This function usually saves to .csv, but remake_figs is off.")
         warning("Printing to screen only.")
         print(docs)
     }
-    
+
     return(docs)
 }
 
@@ -550,11 +558,11 @@ top_titles_table <- function(dataset_name="noexcludes2001_2015",
                             depth=3,        # how many titles per topic?
                             showlabels=TRUE)
 {
-    
+
     if(!is.null(topic)) {
         howmany = 1
     }
-    
+
     ttt <- top_topic_browser(dataset_name=dataset_name,
                            ntopics=ntopics,
                            subset_name=subset_name,
@@ -564,22 +572,22 @@ top_titles_table <- function(dataset_name="noexcludes2001_2015",
                            topic=topic,
                            cutoff=howmany,
                            for.bind=T)
-    
+
     ttt <- ttt[, list(topic, topic_rank, Title, topic_weight, rank_in_doc)]
     head(ttt)
-    
+
     ttt <- ttt[, .(Titles = paste(paste0(Title, " (", 100*round(topic_weight, 2), "%)"), collapse=" || ")), by=list(topic, topic_rank)]
     head(ttt)
-    
-    # TO DO: 
+
+    # TO DO:
     # add in the top keywords by_tfitf (or make it an option to do by_prob),
     # so I don't have to merge by hand in Excel
-    
+
     if(remake_figs) {
         if(!exists ("build_plot_title")) {
             source(file="build_plot_title.R")
         }
-        
+
         outfile <- build_plot_title(dataset_name=dataset_name,
                                     ntopics=ntopics,
                                     iter_index = iter_index,
@@ -589,7 +597,7 @@ top_titles_table <- function(dataset_name="noexcludes2001_2015",
         outfile <- file.path(imageloc, paste0(outfile, ".csv"))
         write.csv(ttt, outfile, row.names = F)
     }
-    
+
     return(ttt)
 }
 
@@ -603,16 +611,16 @@ cluster_titles_table <- function(cluster_members=NULL,    # vector of topic numb
                                  subset_name="knownprograms2001_2015",
                                  depth=3,        # how many titles per topic?
                                  use.labels=TRUE,
-                                 mygrid = NULL,    # pass for slight speed boost 
+                                 mygrid = NULL,    # pass for slight speed boost
                                  verbose = F,
                                  level = 0.5)
 {
     if (! exists("cluster.strength", mode="function")) {
         source(file = "topic cluster reach.R")
     }
-    
+
     this_cluster <- cluster.strength(my.topics = cluster_members,
-                     my.topics_name = cluster_name, 
+                     my.topics_name = cluster_name,
                      dataset_name = dataset_name,
                      ntopics = ntopics,
                      iter_index = iter_index,
@@ -620,37 +628,37 @@ cluster_titles_table <- function(cluster_members=NULL,    # vector of topic numb
                      use.labels = use.labels,
                      level = level,
                      grid = mygrid)
-    
+
     mypubs <- head(this_cluster$docs, depth)
     myweights <- head(this_cluster$doc_levels, depth)
-    
+
     mytable <- data.table(Pub.number = mypubs,
-               ClusterWeight = myweights,
-               key = "Pub.number")
-    
+               ClusterWeight = myweights)
+
     dataset <- data.table(get(dataset_name), key = "Pub.number")
-    
-    mytable[, Title:=dataset[mypubs, Title]]
+
+    mytable[, Title:=dataset[Pub.number, Title]]
     mytable[, "Weight":=round(ClusterWeight, 4) * 100]
-    
+
     to.return <- mytable[order(-ClusterWeight), list(Title, Weight)]
-    
+
     if(verbose) {
-        for (pubnum in mypubs) { 
+        setkey(mytable, "Pub.number")
+        for (pubnum in mypubs) {
             message("Pubnum ", pubnum, ", with cluster at ", mytable[pubnum, Weight])
-            get.topics4doc(pubnum = pubnum, 
-                           showlabels = T, 
+            get.topics4doc(pubnum = pubnum,
+                           showlabels = T,
                            verbose = T
             )
-            readline("<press any key to continue>") 
+            readline("<press any key to continue>")
         }
     }
-    
-    if (remake_figs) { 
+
+    if (remake_figs) {
         if(!exists ("build_plot_title")) {
             source(file="build_plot_title.R")
         }
-        
+
         outfile <- build_plot_title(dataset_name=dataset_name,
                                     ntopics=ntopics,
                                     iter_index = iter_index,
@@ -660,9 +668,9 @@ cluster_titles_table <- function(cluster_members=NULL,    # vector of topic numb
         outfile <- file.path(imageloc, paste0(outfile, ".csv"))
         write.csv(to.return, outfile, row.names = F)
     }
-    
+
     return(to.return)
-        
+
 }
 
 
@@ -692,8 +700,8 @@ if (autorun) {
                   "   ntopics, iter_index, and subset_name, show the top `depth` titles \n",
                   "   for the top `howmany` topics, concatenated with the weight of that topic \n",
                   "   in each doc. \n",
-                  " * cluster_titles_table(cluster_members, cluster_name, ..., howmany, depth): \n", 
-                  "   like top_titles_table(), but for a combined set of topics, treated cumulatively."
+                  " * cluster_titles_table(cluster_members, cluster_name, ..., howmany, depth): \n",
+                  "   like top_titles_table, but for a combined set of topics, treated cumulatively."
     ))
 }
 
@@ -718,21 +726,21 @@ if(FALSE) {
                                   depth=3,        # how many titles per topic
                                   showlabels=TRUE
                       )
-    
+
     bad.topics.table <- cluster_titles_table(dataset_name="noexcludes2001_2015",
                                          ntopics=50,
                                          iter_index=1,
                                          subset_name="knownprograms2001_2015",
                                          depth=3,        # how many titles per topic
-                                         showlabels=TRUE, 
+                                         showlabels=TRUE,
                                          cluster_members = bad.topics
-    ) 
-    
+    )
+
     remake_figs
     cluster_titles_table(cluster_members = c(9, 19, 29, 13, 31),
                          cluster_name = "public rhetoric")
-    
-    
+
+
     # Reconsidering language-based topics. What if they're real?
     shareable_topic(dataset_name = "noexcludes2001_2015",
                     ntopics = 50,
