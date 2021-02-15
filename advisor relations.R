dd8948d7ed9f94e143446b8ca8508ed178c46efa
benmiller314 <benmiller314@gmail.com>
2021-02-15 10:55:00 -0500

    better handling of multiple search columns

--- advisor relations.R
+++ advisor relations.R
@@ -74,42 +74,54 @@ find_person_in_column <- function(person_name,
     if (!exists("namepart", mode="function")) { source(file = "advisor relations.R") }
     person_name <- namepart(person_name, part = 'all', casefunction = titleCase)
     
-    to_search <- sapply(unlist(dataset.dt[, ..search_column]), namepart, part= 'all', casefunction=titleCase)
-    
-    person_index <- grep(person_name, to_search, ignore.case = T)
-    
-    if (length(person_index)) {
-        pubnum <- as.character(unique(dataset.dt[person_index, Pub.number]))
-    } else {
+    person_index <- c()
+    for (i in seq_along(search_column)) {
+        to_search <- sapply(unlist(dataset.dt[, .SD, .SDcols=search_column[i]]), 
+                            namepart, part= 'all', casefunction=titleCase)
+        
+        # to maximize hits, search the name by parts
         tryCatch(expr = {
-            
-            person_names <- namepart(person_name, "list")
-            person_index <- c()
-            for (name in person_names) {
-                person_index <- c(person_index, grep(name, to_search, ignore.case = T))
-            }
-            pubnum <- as.character(unique(dataset.dt[person_index, Pub.number]))
-        },
-        e = "No match for that person; try just last name or first name.",
-        finally = "No exact match found; building list from space-separated name parts."
-        )
-    }
+                person_names <- namepart(person_name, "list", casefunction=titleCase)
+                for (name in person_names) {
+                    person_index <- c(person_index, grep(name, to_search, ignore.case = T))
+                }
+            },
+            error = paste0("No match for person", person_name, "; try just last name or first name."),
+            finally = "No exact match found; building list from space-separated name parts."
+            )
+    } # end of for loop; repeat for all search_column s to build up person_index
+    
+        
+    # get Pub.numbers for every row in person_index, deduplicate
+    pubnum <- as.character(unique(dataset.dt[person_index, Pub.number]))
+
     
     if(length(pubnum) < 1) {
         stop("find_person_in_column: No match for person '", person_name, 
              "' in column '", search_column, "'; try just last name or first name.")
         
     } else if (length(pubnum) > 1) {
-        message("More than one match; please try an exact person name from list below.")
+        message("More than one match:")
         results <- dataset.dt[pubnum, .SD, .SDcols=c(search_column, "Title", "Year",
                                                      "School", "Pub.number")]
+        setkeyv(results, search_column)
         print(results)
         
-        # TO DO: Allow for the case of no correct matches (i.e. skip)
-        a <- as.integer(readline("Use result number... "))
+        message("Please select desired matches for ", person_name, " from list above. \n",
+                "To select multiple, use c(1, 2) or 1:3 notation; to select none, enter 0.")
+            
+        a <- readline("Use result number... ")
+        a <- eval(parse(text = a))
         pubnum <- as.character(results[a, Pub.number])
     }
     
+    # NB: also returns if there was exactly one match
     return(dataset.dt[pubnum])
 }
+
+# # Test values:
+# test1 <- find_person_in_column("Collin Brooke", dataset_name = "noexcludes", search_column = c("Advisor"))
+# test2 <- find_person_in_column("Collin Brooke", dataset_name = "noexcludes", search_column = c("Advisor.Name"))
+# test3 <- find_person_in_column("Collin Brooke", dataset_name = "noexcludes", search_column = c("Advisor", "Advisor.Name"))
+
 
