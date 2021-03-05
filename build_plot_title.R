9f575886006961ae358a9c3bf0b822bae32d4258
benmiller314 <benmiller314@gmail.com>
2021-03-05 12:11:55 -0500

    add function to output one method's collocations with other methods
    as a barplot, with fill color based on method group

--- build_plot_title.R
+++ build_plot_title.R
@@ -5,18 +5,21 @@ build_plot_title <- function(dataset_name, ntopics, iter_index, subset_name=NULL
                              for.filename=FALSE) {
     if(! is.null(subset_name)) { subset_part <- paste0("--", subset_name)
     } else { subset_part <- "" }
+    
+    if(! is.null(ntopics) && is.numeric(ntopics)) { ntopics_part <- paste0("k", ntopics)
+    } else { ntopics_part <- ""}
 
     if(length(bad.topics > 0)) { bad_topics_part <- paste0(", ", length(bad.topics), " bad topics hidden")
     } else { bad_topics_part <- "" }
 
-    if(iter_index > 0) { iter_part <- paste0("_iter", iter_index)
+    if(!is.null(iter_index) && iter_index > 0) { iter_part <- paste0("_iter", iter_index)
     } else { iter_part <- "" }
 
     if(use.labels) { label_part <- ", topics labeled"
     } else { label_part <- "" }
 
     main <- paste0(whatitis, ", ",                                       # type of plot/file
-                   dataset_name, "k", ntopics, iter_part,                # which topic model
+                   dataset_name, ntopics_part, iter_part,                # which topic model
                    subset_part, bad_topics_part,                         # parts of that model
                    label_part)                                           # topics named or numbered
 
--- method collocation heatmap.R
+++ method collocation heatmap.R
@@ -119,7 +119,7 @@ sumbytags <- function(dataset_name = "noexcludes",
 	    }
 	    
 	    # actually build the map
-		heatmap.ben(dataset_name = dataset_name,
+		mapvals <- heatmap.ben(dataset_name = dataset_name,
 		            tagset_name = tagset_name,
 		            sum.by.tags = to.return, 
 		            rowscale = normed,
@@ -127,6 +127,7 @@ sumbytags <- function(dataset_name = "noexcludes",
 		            dendro = dendro,
 		            mytitle = paste0(slug, ", ", dataset_name, " N", nrow(dataset)))
 	
+		to.return <- c(to.return, mapvals)
     } # end of if(doplot)
 
     return (to.return)
@@ -134,19 +135,94 @@ sumbytags <- function(dataset_name = "noexcludes",
 } # end of wrapper function sumbytags()
 
 
+one_method_corrs_barplot <- function(onetag,
+                                     dataset_name = "knownprograms2001_2015",
+                                     tagset_name = "no_ped_tagnames",
+                                     taggroups = no_ped_taggroups,
+                                     normed = F,
+                                     method_corrs = NULL # pass for speed boost if it exists
+){
+    if(is.null(method_corrs$rowInd)) {
+        method_corrs <- sumbytags(dataset_name, tagset_name, 
+                                  doplot=T, 
+                                  normed=normed, 
+                                  dendro=T)
+    }
+    
+    myorder <- method_corrs$colInd
+    
+    tagset <- get(tagset_name)
+    mygroups <- taggroups[names(taggroups) %in% tagset][myorder]
+    
+    # get group labels in the right order, assign them a color palette
+    require(viridisLite)
+    group_pal <- viridis(4+length(unique(mygroups)))
+    if(length(group_pal) > 8) {
+        group_pal <- c(group_pal[c(3, 6, 8, 9)], "#FFFFFF")
+    } 
+    
+    if(!exists("get_tags", mode="function")) { source(file="get tags.R") }
+    mysort <- order(get_tags(dataset_name=dataset_name, tagset_name=tagset_name), 
+                    decreasing = T)
+    names(group_pal) <- unique(taggroups[mysort])
+    
+    group_pal
+    
+    group_pal[mygroups[names(method_corrs$correlations[onetag, myorder])]]
+    
+    if(!exists("build_plot_title", mode="function")) {
+        source(file = "build_plot_title.R")
+    }
+    maintitle <- build_plot_title(dataset_name = dataset_name,
+                                  subset_name = tagset_name,
+                                  ntopics = NULL,
+                                  iter_index = NULL,
+                                  whatitis = paste("Method correlations for", onetag),
+                                  for.filename = remake_figs)
+    maintitle <- sub(",", "\n", maintitle)
+    
+    barplot(method_corrs$correlations[onetag, myorder],
+            col = group_pal[mygroups[names(method_corrs$correlations[onetag, myorder])]],
+            main = maintitle)
+    
+}
+
+
+
 if (!autorun) {
-    message("One function loaded: \n",
-            "sumbytags(dataset_name, tagset_name, doplot=T, normed=F, dendro=F)")
+    message("The following functions have been loaded: \n",
+            " sumbytags(dataset_name, tagset_name, doplot=T, normed=F, dendro=F)\n",
+            " one_method_corrs_barplot(onetag, dataset_name, tagset_name, normed=F, method_corrs=NULL)")
 }
 
 # Testing area
 if (FALSE) { 
 	remake_figs=F
 	
-    sumbytags("knownprograms2001_2015", "no_ped_tagnames", 
+	dataset_name <- "knownprograms2001_2015"
+	tagset_name <- "no_ped_tagnames"
+	tagset <- get(tagset_name)
+	taggroups <- no_ped_taggroups
+	
+	
+    method_corrs <- sumbytags(dataset_name, tagset_name, 
               doplot=T, 
-              normed=F, 
+              normed=T, 
               dendro=T)
+    
+    for(tag in tagset) {
+        one_method_corrs_barplot(onetag = tag,
+                                 dataset_name = dataset_name,
+                                 tagset_name = tagset_name,
+                                 taggroups = taggroups,
+                                 method_corrs = method_corrs)    
+    }
+    
+    
+    # test colors: viridis do distinguish methodological groups, magma for values
+    require(viridisLite)
+    mypal <- magma(19, direction = -1)
+    mypal <- c("#FFFFFF", mypal)
     
     sumbytags("knownprograms2001_2015", "tagnames", 
               doplot=T, 
