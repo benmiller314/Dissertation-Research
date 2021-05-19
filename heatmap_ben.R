62ed8728d949f0b9b0746f000278a2cc5132f5ac
benmiller314 <benmiller314@gmail.com>
2021-05-18 22:38:31 -0400

    many improvements
    - allow different sorting methods
    - allow custom color palettes
    - allow full control of column and row order (e.g. for comparisons)
    - return more info to feed into the above

--- heatmap_ben.R
+++ heatmap_ben.R
@@ -14,8 +14,10 @@
 heatmap.ben <- function (
 	dataset_name = "noexcludes2001_2015",
 	tagset_name = "tagnames",
-	sum.by.tags = NULL,    # Allow for passing in correlations from sumbytags; avoid infinite loop!
-    diags    = FALSE,		# Should we outline diagonals? 
+	sum.by.tags = NULL,     # Allow for passing in correlations from sumbytags; avoid infinite loop!
+    diags    = FALSE,		# Should we outline diagonals?
+	palette  = NULL,        # Optionally pass a vector of hex colors or a generator function
+	                        # NB: if palette is a vector, numCols will be ignored.
 	highval  = "#818181",	# Darkest color
 	lowval   = "#FAFAFA",	# Lightest color
 	numCols  = 10,			# How many different shades?
@@ -25,8 +27,10 @@ heatmap.ben <- function (
 							# for the color map?
 	dendro	 = FALSE,		# Should we output dendrograms showing method
 							# clustering?
-	mytitle    = NULL,         # optionally pass a title for the plot
-	clust.method = "agnes"  # any option from cluster package
+	mytitle    = NULL,      # optionally pass a title for the plot
+	clust.method = hclust,  # or any option from cluster package, like agnes or diana
+	rowInd = NULL,          # use for replicating row order across samples
+	colInd = NULL           # use for replicating column order across samples
 	) 
 {
     
@@ -35,13 +39,24 @@ heatmap.ben <- function (
     # get the data
      
     if (!is.null(sum.by.tags)) {
-        if((dataset_name != sum.by.tags$dataset_name) ||
-           (tagset_name != sum.by.tags$tagset_name)) {
-            warning("heatmap.ben: dataset or tagset mismatch.\n",
-                    "Using values from passed correlation matrix.")
+        
+        # if it exists and is a matrix, move on!
+        # if it's not a matrix, extract dataset/tagset info from it.
+        if(!is.matrix(sum.by.tags)) {
+            # check if it's from the right dataset
+            if((dataset_name != sum.by.tags$dataset_name) ||
+               (tagset_name != sum.by.tags$tagset_name)) {
+                warning("heatmap.ben: dataset or tagset mismatch.\n",
+                        "Using values from passed correlation matrix, ",
+                        "but filenames may be inaccurate. \n", 
+                        "Internal remake_figs set to FALSE; save manually or fix mismatch.")
+                remake_figs <- F
+            }
+            dataset_name <- sum.by.tags$dataset_name
+            tagset_name <- sum.by.tags$tagset_name
         }
-        dataset_name <- sum.by.tags$dataset_name
-        tagset_name <- sum.by.tags$tagset_name
+        
+    # if the data doesn't exist, create it now.     
     } else {
         if(!exists("sumbytags", mode="function")) {
             source(file="sum by tags.R")
@@ -68,21 +83,25 @@ heatmap.ben <- function (
 	## sort the matrix (borrowed from heatmap())
 	
 	Rowv <- rowMeans(sum.by.tags.s, na.rm = TRUE)	# Find row means
-	hcr <- hclust(dist(sum.by.tags.s))				# Cluster based on distances
+	hcr <- clust.method(dist(sum.by.tags.s))				# Cluster based on distances
     ddr <- as.dendrogram(hcr)						# Convert to dendrogram 
 													# (which we might use later)
    	ddr <- reorder(ddr, -Rowv)						# Reorder the dendrogram, 
-   	                                                # highest values on top
-    rowInd <- order.dendrogram(ddr)					# Extract the new row order 
+   	
+   	if(is.null(rowInd)) {                                                # highest values on top
+        rowInd <- order.dendrogram(ddr)					# Extract the new row order 
+   	}
    	Colv <- colMeans(sum.by.tags.s, na.rm = TRUE)	# Find column means
-    hcc <- hclust(dist( if(symm) {sum.by.tags.s}	# Cluster based on distances 
+    hcc <- clust.method(dist( if(symm) {sum.by.tags.s}	# Cluster based on distances 
 													# (from the col perspective)
   						else {t(sum.by.tags.s)}))
 	ddc <- as.dendrogram(hcc)						# Convert to dendrogram 
 													# (which we might use later)
 	ddc <- reorder(ddc, -Colv)						# Reorder the dendrogram, 
-	                                                # highest values at left
-	colInd <- order.dendrogram(ddc)					# Extract the new column order
+	
+	if(is.null(colInd)) {                                            # highest values at left
+	    colInd <- order.dendrogram(ddc)					# Extract the new column order
+	}
 	
 	sum.by.tags.s <- sum.by.tags.s[rowInd, colInd]	# Apply row and column 
 													# orders from above
@@ -110,23 +129,48 @@ heatmap.ben <- function (
 		sapply(sum.by.tags.s, FUN=function(x) { if(is.na(x)) x <- 0 })
     }
 
-	# color function
-		# if we're norming rows, use white for 0 and black for 100%
-		if(rowscale) {				
+	## color function
+	
+	
+    # if we're norming rows, override defaults and use white for 0 and black for 100%
+		if (rowscale) {				
 			max.val <- 1
 			min.val <- 0
 			highval <- "#000000"
 			lowval  <- "#FFFFFF"
 		} else {
-			if(any(sum.by.tags.s == 0)) { lowval <- "#FFFFFF" }
-			max.val <- max(sum.by.tags.s)
+		    max.val <- max(sum.by.tags.s)
 			min.val <- min(sum.by.tags.s)
 		}
-
-		pal <- colorRampPalette(c(lowval, highval))
-		cols <- pal(numCols)
+	
+	# but if a palette already exists, use it.
+    	if (is.null(palette)) { 
+    	    pal <- colorRampPalette(c(lowval, highval))
+    	    cols <- pal(numCols)
+    	} else {
+    	    # if the palette isn't a function, let's hope it's a vector of colors...
+    	    if (mode(palette) == "function") {
+        	    pal <- palette 
+    	        cols <- pal(numCols)
+    	    } else if (mode(palette) == "character") {
+        	    cols <- palette
+    	    } else {
+    	        warning("heatmap.ben: palette was passed, but isn't a function or vector of hex values;\n",
+    	                "proceeding with default values.")
+    	        pal <- colorRampPalette(c(lowval, highval))
+    	        cols <- pal(numCols)
+        	}
+    	}
+	
+	
 
 	colorme <- function (val) {
+	    # make sure actual zero values have blank/white backgrounds
+	    if(val == 0) { return ("#FFFFFF") }
+	    
+	    # otherwise, use the palette defined above
+	    # TO DO: ugh, forgot to account for negative numbers. Maybe a uniform shift up by abs(min.val)?
+	    
 		colIndex <- round(numCols * (val - min.val) / (max.val - min.val))
 		colIndex <- max(1,colIndex)
 		return(cols[colIndex])
@@ -284,10 +328,17 @@ heatmap.ben <- function (
 		}
 	} # end of if(dendro)
 	
+	# return stuff you might use (e.g. for duplicating this row/column order)
+	invisible(list(plottedData = sum.by.tags.s,
+	               rowInd = rowInd, 
+	               colInd = colInd, 
+	               Rowv = ddr, 
+	               Colv = ddc))
+	
 } # end of wrapper function heatmap.ben()
 
 if(FALSE) {
-    heatmap.ben(dataset_name="noexcludes2001_2015", 
+    a <- heatmap.ben(dataset_name="noexcludes2001_2015", 
                 tagset_name="no_ped_tagnames",
                 diags=T,
                 rowscale=T)
