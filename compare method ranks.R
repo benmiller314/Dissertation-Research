37eae175c6e5072cd550f30806e98794ac444c32
benmiller314 <benmiller314@gmail.com>
2021-06-14 15:07:20 -0400

    new function: method_line_graph
    still TO DO: color by method group, small multiples on one plot

--- compare method ranks.R
+++ compare method ranks.R
@@ -219,9 +219,156 @@ compare_method_ranks <- function(set1="consorts",
 }		# end of wrapper function compare_method_ranks
 
 
+# Plot changes in a given set of methods over time.
+method_line_graph <- function(tagset = no_ped_tagnames,     # character vector of method tags
+                              dataset = knownprograms2001_2015,
+                              start_year = 2001,
+                              end_year = 2015, 
+                              scaled = FALSE,
+                              do.plot = TRUE, 
+                              label_year_n = TRUE,
+                              mycolors = NULL,
+                              ... )   # graphing parameters
+{
+    # Placeholders for data
+    tagcounts <- c()
+    yearspan <- length(start_year:end_year)
+    all_year_n <- c()
+    
+    # Tweak sum function depending on length of tagset
+    sumfun <- if(length(tagset) == 1) sum else colSums
+    
+    # Loop over years, gathering data into place
+    for (year in start_year:end_year) {
+    
+        yearcounts <- sumfun(dataset[which(dataset$Year == year), tagset])
+        year_n <- length(dataset[which(dataset$Year == year), "Year"])
+        if(scaled) {
+            yearcounts <- yearcounts / year_n
+        }
+        tagcounts <- cbind(tagcounts, yearcounts)
+        all_year_n <- c(all_year_n, year_n)
+    }
+    
+    # Label the years
+    colnames(tagcounts) <- start_year:end_year
+    names(all_year_n) <- start_year:end_year
+    
+    # If only one method, have to add that name
+    if(is.null(row.names(tagcounts))) {
+        row.names(tagcounts) <- tagset
+    }
+
+    # If we're using percentages, present it neatly    
+    if(scaled) {
+        tagcounts <- round(100*tagcounts, 2)
+    }
+    
+    if(remake_figs) {
+        if(!exists("build_plot_title")) {
+            source(file = "build_plot_title.R")
+        }
+        outfile <- paste("method line graph", 
+                          # tagset
+                          if(length(tagset) > 10) {
+                              substitute(tagset)
+                          } else if (length(tagset) == 1) { 
+                              tagset 
+                          } else { 
+                              paste0(tagset, sep="-") 
+                          },
+                         
+                          # year span
+                          start_year, "-", 
+                          end_year,
+                         
+                          # scaled?
+                          if(scaled) "scaled" else ""
+                         )
+        
+                                   
+        outfile <- paste0(outfile, ".pdf")
+        outfile <- file.path(imageloc, outfile)
+        
+        pdf(outfile)
+    }
+    
+    # Create empty plot using max value + some extra space
+    plot(NULL,
+         xlim = c(0, yearspan + 1),
+         ylim = c(0, max(tagcounts)),
+         bty = "n",
+         xaxt = "n",
+         xlab = "Year",
+         ylab = paste0(if(scaled) "% " else "", "Dissertations Tagged")
+    )
+    
+    # Label the years along the x-axis
+    axis(side=1, at=1:yearspan, labels=colnames(tagcounts))
+
+    # And optionally the number of dissertations for each year    
+    if(label_year_n) {
+        axis(side=1, at=1:yearspan, labels=paste0("n=",all_year_n),
+             tick = F,
+             line = 1,
+             lwd = 0,
+             cex.axis = 0.7,
+             )
+    }
+    
+    # If no colors are provided, use pretty colors
+    if(is.null(mycolors)) {
+        require(viridisLite)
+        mycolors <- viridis(n = length(tagset))
+    }
+    
+    # Use preferred labels to distinguish lines
+    mypch <- c(1, 2, 0, 15:18, 3:7, 10, 13, 12)
+    
+    
+    for (i in seq_along(tagset)) {
+        points(tagcounts[tagset[i],], bty="n", xaxt="n", 
+               col = mycolors[i],
+               pch = mypch[i])
+        lines(lowess(tagcounts[tagset[i],], f=1/2), 
+              col = mycolors[i])
+    }
+    
+    outside_legend("topright", 
+                   legend = tagset, 
+                   col = mycolors, 
+                   pch = mypch,
+                   bty = "l")
+    
+    if(remake_figs) {
+        dev.off()
+    }
+    
+    return(tagcounts)
+}
+
+
+
 # Testing area
 if(FALSE) {   
 	remake_figs=F
+
+	method_line_graph(tagset=c("Ethn"), mycolors = "black")
+	method_line_graph(tagset=c("Rhet", "Meta", "Clin"))
+	
+	method_line_graph(tagset=no_ped_tagnames)
+	
+	remake_figs=T
+    	require(viridisLite)
+    	mycolors <- viridis(n = length(no_ped_tagnames))
+    	# TO DO: use method-group colors as per chapter 3
+    	# TO DO: make small multiples plot with all methods
+    	
+    	for (i in seq_along(no_ped_tagnames)) {
+    	    method_line_graph(tagset=c(no_ped_tagnames[i]), mycolors = mycolors[i])
+    	}
+	remake_figs=F
+	
 	compare_method_ranks("consorts", "nonconsorts", 
 						betterlabels=c("Consortium", "All Non-Consortium"))
 	compare_method_ranks("consorts", "top.nonconsorts", 
@@ -238,6 +385,8 @@ if(FALSE) {
 	compare_method_ranks("knownprograms2001_2015", "nonconsorts2001_2015")
 	compare_method_ranks("knownprograms2001_2005", "knownprograms2011_2015", tagset_name="tagnames.simple")
 	
+	
+	# RCWS vs non-RCWS
 	compare_method_ranks("knownprograms2001_2015", 
 	                     "nonrcws2001_2015", 
 	                     tagset_name="no_ped_tagnames",
@@ -247,11 +396,61 @@ if(FALSE) {
 	                     set2="nonrcws2001_2015sans_badtops", 
 	                     betterlabels=c("Confirmed RCWS dissertations", 
 	                                    "Confirmed non-RCWS dissertations"), 
-	                     tagset_name="no_ped_tagnames")
+	                     tagset_name="no_ped_tagnames", verbose = T)
 	
+	# Different time bins
+	    # no significant differences between first and second third
+	compare_method_ranks("knownprograms2001_2005",
+	                     "knownprograms2006_2010",
+	                     tagset_name="no_ped_tagnames",
+	                     betterlabels=c("RCWS 2001-2005", "RCWS 2006-2010"))
 	
+	    # in final third, highly significant increases in Rhet and Crit; all else not significant.
+	compare_method_ranks("knownprograms2006_2010",
+	                     "knownprograms2011_2015",
+	                     tagset_name="no_ped_tagnames",
+	                     betterlabels=c("RCWS 2006-2010", "RCWS 2011-2015"))
 	
+	    # what about first third compared to last third?
+	compare_method_ranks("knownprograms2001_2005",
+	                     "knownprograms2011_2015",
+	                     tagset_name="no_ped_tagnames",
+	                     betterlabels=c("RCWS 2001-2005", "RCWS 2011-2015"),
+	                     verbose = T)
+	    # now there are more changes:
+        #   Rhet jumps from 16% (sixth place) to 49% (first place), p < 0.001
+	    #   Crit climbs from 26% (third place) to 41% (second place), p < 0.001
+	    #   Intv increases modestly from 8% to 14% (p = 0.0015)
+	    #   Meta falls from 9% to 2% (last place) (p = 0.000025)
+	
+	for(startyear in 2001:2014) {
+	    endyear <- startyear
+	    compare_method_ranks(set1 = paste0("knownprograms", startyear, "_", endyear),
+	                         set2 = paste0("knownprograms", startyear+1, "_", endyear+1),
+	                         tagset_name = "no_ped_tagnames",
+	                         betterlabels = c(paste0("RCWS ", startyear, "-", endyear),
+	                                         paste0("RCWS ", startyear+1, "-", endyear+1))
+	                        )
+	}   # going year by year, the only significant jumps are in Rhet: * in 2010-11, and ** in 2011-12
+	
+	for(startyear in 2001:2014) {
+	    endyear <- startyear + 1
+	    compare_method_ranks(set1 = paste0("nonrcws2001_2015sans_badtops", startyear, "_", endyear),
+	                         set2 = paste0("nonrcws2001_2015sans_badtops", startyear+1, "_", endyear+1),
+	                         tagset_name = "no_ped_tagnames",
+	                         betterlabels = c(paste0("Non-RCWS ", startyear, "-", endyear),
+	                                          paste0("Non-RCWS ", startyear+1, "-", endyear+1))
+	    )
+	}   # going year by year, the only significant jumps are in Rhet: ** in 2011-12
+	
+	compare_method_ranks("knownprograms2001_2002",
+	                     "knownprograms2014_2015",
+	                     tagset_name="no_ped_tagnames",
+	                     betterlabels=c("RCWS 2001-2002", "RCWS 2014-2015"),
+	                     verbose = T)
+	method_line_graph()
 } else {
-    message("The following function has been loaded:")
-    message("    compare_method_ranks(set1, set2, pcts, colorful, betterlabels, tagset_name)")
+    message("The following function has been loaded:\n",
+            "    compare_method_ranks(set1, set2, pcts, colorful, betterlabels, tagset_name) \n",
+            "    method_line_graph(tagset, dataset, start_year, end_year, scaled, ...)")
 }
