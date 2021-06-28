5cee1a3705735c9dffd0adc44c90666e3ad6862c
benmiller314 <benmiller314@gmail.com>
2021-06-28 11:29:10 -0400

    update instructions for installing aspell on mac
    (i.e. use Homebrew)

--- Shell scripts and commands/ben_clean_and_consolidate.sh
+++ Shell scripts and commands/ben_clean_and_consolidate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash                             # declare our shell environment
 
-##### 
+#####
 # ben_clean_and_consolidate.sh
-# 
+#
 # GOAL: given pdf files from which you want to build a topic model, extract
 # the full text and remove boilerplate that could skew your results.
 #
-# STRATEGY: Read in a list of files in a directory. 
-# While items remain in the list, run a series of commands on them, 
+# STRATEGY: Read in a list of files in a directory.
+# While items remain in the list, run a series of commands on them,
 # saving the results into new files to avoid accidental overwriting.
 # Based on scripts by Micki Kaufman (https://twitter.com/MickiKaufman).
 #
@@ -20,7 +20,7 @@
 #####
 
 
-# Declare some basics: source and destination. 
+# Declare some basics: source and destination.
 # NB: these will likely change often!
 DATASET=$1
 # PDF="/Users/benmiller314/Documents/fulltext_dissertations/morepdfs"
@@ -45,7 +45,7 @@ CUMUL=~/"Documents/fulltext_dissertations/cumulative"
 
 
 ## Zeroth function: extract text from pdf. Run in the $PDF folder.
-#  NB: pdftotext is standard on linux distributions; a mac port 
+#  NB: pdftotext is standard on linux distributions; a mac port
 #  is available for free from http://www.bluem.net/en/mac/packages/
 function extract()
 {
@@ -56,26 +56,26 @@ function extract()
 
     # Start the loop.
     while read line1; do
-		# strip filename extension: awk creates a table divided at 
-		# the field separator (FS); we want the first "cell," 
+		# strip filename extension: awk creates a table divided at
+		# the field separator (FS); we want the first "cell,"
 		# i.e. whatever comes before the period.
         PUB=`printf $line1 | awk 'BEGIN { FS="." } { print $1; }'`
-        
+
         # progress report
-        printf "Converting $line1 to $PUB.txt ... "             
-        
+        printf "Converting $line1 to $PUB.txt ... "
+
         # convert the file.
-        pdftotext "$PDF/$line1"                                 
-        
+        pdftotext "$PDF/$line1"
+
         # progress report
-        if [ $? = 0 ] ; then printf "File made " ; fi           
-        
+        if [ $? = 0 ] ; then printf "File made " ; fi
+
         # move to txt folder.
-        mv "$PDF/$PUB.txt" "$SRC/$PUB.txt"                      
-        
+        mv "$PDF/$PUB.txt" "$SRC/$PUB.txt"
+
         # progress report
-        if [ $? = 0 ] ; then echo "and moved." ; fi             
-    
+        if [ $? = 0 ] ; then echo "and moved." ; fi
+
     # Close the loop.
     done
 
@@ -84,7 +84,7 @@ function extract()
 
 ## First function: Get text that R can read. Run in the $SRC folder.
 function clean ()
-{   
+{
 	# Make sure we have a place to output to.
 	if ! [ -d "$DST" ] ; then
 		mkdir -p "$DST"
@@ -95,12 +95,12 @@ function clean ()
 
 	## Step 1. Copy the file to a new directory, making changes as it goes
 	echo "Cleaning from SRC $line1 to DST $DST/$line1"		# progress report
-	
+
 	# 1a. Convert text encoding from ISO 8859-1 (Latin-1) to UTF-8 (unicode standard)
-	# 1b. Using tr, delete all non-line-break non-Western characters 
+	# 1b. Using tr, delete all non-line-break non-Western characters
 	#     OR IS THIS SOLVED BY CONVERTING TO UTF-8?
-	# 1c. Using sed, delete the first page added by UMI (which starts in line 1, and 
-	#	  usually ends with the zip code) TO DO: BE SMARTER ABOUT THIS REGEX. 
+	# 1c. Using sed, delete the first page added by UMI (which starts in line 1, and
+	#	  usually ends with the zip code) TO DO: BE SMARTER ABOUT THIS REGEX.
 	#	  Try removing everything from INFORMATION to the line in 1d?
 	# 1d. Using sed, remove all instances of the line below:
 	#	  "Reproduced with permission of the copyright owner. Further reproduction
@@ -113,68 +113,68 @@ function clean ()
     sed "1,/Further reproduction prohibited without permission./d" | \
     sed "/Reproduced with permission of the copyright owner. Further reproduction prohibited without permission./d"
 	> "$DST/cleaned_$line1"
-	
+
 	# catch the case where we've stripped too much (ie. the file has 0 bytes)
 	# and do it again without sed
 		if ! [ -s "$DST/cleaned_$line1" ] ; then
 			iconv -f ISO_8859-1 -t UTF-8 "$SRC/$line1" | \
 			tr -cd '\11\12\40-\176' > "$DST/cleaned_$line1"
 		fi
-	
+
 	# Close the loop.
 	done
-	
+
 	# Close the function.
 }
 
 ## Second function: Combine files into a big cumulative one. Here's how:
 function combine ()
-{   
+{
     # Step 1. Outside the loop, create an empty file to hold the cumulative
-    # output. 
+    # output.
     if ! [ -e "$CUMUL/""$DATASET""_cumulative.txt" ] ; then
         printf '' > "$CUMUL/""$DATASET""_cumulative.txt"
-    else 
+    else
         echo "ERROR: $CUMUL/""$DATASET""_cumulative.txt already exists; aborting combine step."
         exit 1
     fi
-    
+
     echo "Making cumulative file:"              # progress report
-    
+
     # Step 2. Concatenate the cleaned file (after removing line breaks) and
     # append it to the cumulative output file in MALLET-ready format, as per
     # http://mallet.cs.umass.edu/import.php: "The first token of each line
     # (whitespace delimited, with optional comma) becomes the instance name,
     # the second token becomes the label, and all additional text on the
     # line is interpreted as a sequence of word tokens."
-    
+
     while read line1; do
         printf "adding $line1..."               # progress report
-    
+
         # 2a. Using awk, strip '.txt' and 'cleaned_' off the filename to get
         # the Pub.number of the diss. We'll use these as instance names in
-        # MALLET.       
+        # MALLET.
         PUB=`printf $line1 | awk 'BEGIN { FS="." } { print $1; }' | awk 'BEGIN { FS="_" } { print $2; }'`
         # 2b. We don't have labels right now, but eventually we could use
         # methods exported from R.
         LAB="placeholder"
-        
+
         # 2c. Using tr, remove all commas so we can get a clean csv,
         # then replace newlines with spaces (get all text on one line).
-        CONTENTS=`cat "$DST/$line1" | tr -d "," | tr -s '\n' " "`       
-        
+        CONTENTS=`cat "$DST/$line1" | tr -d "," | tr -s '\n' " "`
+
         # 2d. String together the instance names, labels, and the file
         # contents;
         echo "$PUB $LAB $CONTENTS" >> "$CUMUL/""$DATASET""_cumulative.txt"
-        
+
         if [ $? = 0 ] ; then                    # progress report
             echo "done."
         fi
     done
-    
-    if [ $? = 0 ] ; then 
+
+    if [ $? = 0 ] ; then
         # progress report
-        echo "All text saved to $CUMUL/""$DATASET""_cumulative.txt"     
+        echo "All text saved to $CUMUL/""$DATASET""_cumulative.txt"
         echo ''
     fi
 }
@@ -182,14 +182,14 @@ function combine ()
 
 ## Third function: Get data toward a conservative estimate of OCR accuracy.
 ##
-## Strategy: for each file "$line1" in a directory index (produced by ls), 
+## Strategy: for each file "$line1" in a directory index (produced by ls),
 ## (1) find the wordcount
 ## (2) run a spellcheck, and save errors to a file
 ## (3) count the number of errors in that file
 ## (4) compile into a single file for further processing in R
 
 function spellcount ()
-{       
+{
 # (step 0 or 4a) Outside the loop, create a placeholder output file
 if ! [ -d "$DST/_spellstats" ] ; then
     mkdir "$DST/_spellstats"
@@ -197,7 +197,7 @@ fi
 
 if ! [ -e "$DST/_spellstats/spellstats.csv" ] ; then
     echo 'Pub.Number, WordCount, ErrorCount' > "$DST/_spellstats/spellstats.csv"
-else 
+else
     echo "spellstats.csv already exists; aborting script to avoid duplication."
     echo "To append, use new DST folder and concatenate later."
     exit 1
@@ -212,16 +212,9 @@ while read line1; do
 
 ## (step 2) Find misspelled words, save to file in case we want to analyze
 ## later.
-# UPDATE: Ignore the NB below. Easiest way to install on a Mac is with a package manager 
-# like Homebrew. First, go to http://brew.sh and use the simple installation script. 
+# To install the aspell utility on a Mac, use a package manager like Homebrew.
+# If you don't have Homewbrew, go to http://brew.sh and use the simple installation script.
 # Then simply type this at a Terminal prompt (without quotes): "brew install aspell"
-# 	NB: apparently this isn't included in OS X 10.7 (Lion). Boo. To download
-# 		the aspell command, you'll need something like Fink
-# 		http://www.finkproject.org/download/srcdist.php and Apple Developer Command
-# 		Line Tools https://developer.apple.com/downloads/index.action and
-# 		ftp://ftp.gnu.org/gnu/aspell/dict/en/aspell6-en-7.1-0.tar.bz2 for
-# 		dictionaries. Once those are installed (no small feat), uncomment and run
-# 		the commands in file "install aspell dictionary.txt" 
 
     aspell list < "$DST/$line1" > "$DST/_spellstats/wordswrong_$line1"
 
@@ -232,9 +225,9 @@ while read line1; do
 ## (step 4) combine files into a cumulative table. Here's how:
     # Step 4a. Outside the loop, create a placeholder output file. (See
     # above.)
-    
+
     # Step 4b. Strip '.txt' and 'cleaned_' off the filename; this will help
-    # us join tables later.     
+    # us join tables later.
     PUB=`printf $line1 | awk 'BEGIN { FS="." } { print $1; }'` #| awk 'BEGIN { FS="_" } { print $2; }'`
     # Step 4c. String together the Pub.number, the wordcount, and the
     # errorcount; append to the output file.
@@ -243,10 +236,10 @@ while read line1; do
 
 ## Close the loop
 done
-    
+
     # Final report: Tell us what we've got!
     echo "Spelling counts saved to $DST/_spellstats/spellstats.csv."
-    
+
     FILECOUNT=`wc -l "$DST/_spellstats/spellstats.csv" | awk '{ print $1; }' - `
     let FILECOUNT=$FILECOUNT-1          # account for headers in 1st line
     echo "$FILECOUNT files processed."
@@ -262,12 +255,12 @@ done
 ## Go to the files, and run all the functions.
 ## IMPORTANT: Comment out those you don't need right now.
 CURRENT_DIR=$PWD
-# cd "$PDF"                                         # Go to pdf directory
-# \ls *.pdf | extract                                # Call 0th function
+cd "$PDF"                                         # Go to pdf directory
+\ls *.pdf | extract                                # Call 0th function
 cd "$SRC"                                         # Go to source directory
 \ls *.txt | clean                                  # Call 1st function
 cd "$DST"                                           # Go to output directory
-# \ls cleaned* | combine                               # Call 2nd function
-# \ls cleaned* | spellcount                          # Call 3rd function
-# \ls *.txt | spellcount
+\ls cleaned* | combine                               # Call 2nd function
+\ls cleaned* | spellcount                          # Call 3rd function
+\ls *.txt | spellcount
 cd "$CURRENT_DIR"                                   # Go back where we were
