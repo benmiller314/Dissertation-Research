5be9f61a96c95507a9371988d6c912c3fbfb7598
benmiller314 <benmiller314@gmail.com>
2022-01-18 15:17:20 -0500

    clean up subset assignments

--- dataprep 2 - load data.R
+++ dataprep 2 - load data.R
@@ -2,7 +2,7 @@
 ################################################################################
 # dataprep 2 - load data.R
 #
-# A file to read in dissertation metadata from a csv file. Binds key subsets 
+# A file to read in dissertation metadata from a csv file. Binds key subsets
 # of data to variables and encodes method tags for easier analysis.
 #
 # NB: To identify schools in the Consortium of Doctoral Programs in Rhetoric and
@@ -13,17 +13,17 @@ if (!exists("tagnames")) {
     source(file=file.path("~","Box Sync", "research", "dissertations", "data, code, and figures", "Dissertation-Research", "dataprep.R"))
 }
 
-## now get the data 
+## now get the data
 
 if(dual_source) {
     # The original file of dissertation metadata (built from 2012-2015, covering years 2001-2010)
     # oldarray <- read.csv(file=file.path(dataloc, "Rhetoric-or-Composition-12-adding-depts.csv"))
     oldarray <- read.csv(file=file.path(dataloc, "Diss-data-collected-pre-Pitt_short-tags.csv"))
-    
+
     # parse the method tags... including for the collapsed schema. parse_tags() is from `method tag array.R`.
     oldarray <- parse_tags(oldarray, tagstyle="short", excludecol="Flags")
     oldarray <- short_schema(oldarray)
-    
+
 }
 
 # The new file of dissertation metadata
@@ -33,7 +33,7 @@ newarray <- read.csv(file=newdatafile)
 
 
 
-# parse the method tags... including for the collapsed schema. 
+# parse the method tags... including for the collapsed schema.
 # parse_tags() is from `method tag array.R`. short_schema() is from 'simplifying the schema.R'
 # both files are sourced in `dataprep.R`.
 newarray <- parse_tags(newarray, tagstyle="short", excludecol="Flags")
@@ -49,34 +49,34 @@ if(dual_source) {
 }
 
 # filter out false positives
-noexcludes <- bigarray[bigarray$Exclude.Level==0,] 
+noexcludes <- bigarray[bigarray$Exclude.Level==0,]
 justexcludes <- bigarray[bigarray$Exclude.Level>0,]
 
-diss.count <- nrow(noexcludes) 
+diss.count <- nrow(noexcludes)
 false.positives <- nrow(justexcludes)
 
 message(paste("In this data set, there are",diss.count,"dissertations, not counting",false.positives,"false positives."))
 
-# mark noexcludes that will remain in for abstracts, but that 
+# mark noexcludes that will remain in for abstracts, but that
 # we'll leave out of full-text analysis (non-English, bad scans);
 # these will now have Exclude.Level=3
 noexcludes <- remove_unreadables("noexcludes", more=c("3715615", "3537773",
                                                       "3022956", "3454925",
-                                                      "3457750", "3719278", 
+                                                      "3457750", "3719278",
                                                       "3407559", "3671259",
                                                       "3684306", "3701093",
                                                       "3530126", "3506182",
                                                       "3010241"))
 
 # noexcludes <- noexcludes[which(noexcludes$Exclude.Level == 0),]
-    
+
 # refactor levels for noexcludes alone
 refactor.index <- which(names(noexcludes) %in% c("Subject","KEYWORDS","School","Advisor.type","Advisor.Name","Degree","Method.Terms","pages","Flag.notes"))
 for (i in refactor.index) {
 	noexcludes[,i] <- factor(noexcludes[,i])
 }
 
-# TO DO: Make this into a function without weird side effects; 
+# TO DO: Make this into a function without weird side effects;
 # 'check count.R' seems to be misaligning Method.Terms, so the tagarray doesn't line up afterwards
 # # redefine methods that are all "check" or "check?" as "Other," and recalculate "Method.Count"
 # source(file=file.path(sourceloc, "check count.R"))
@@ -94,14 +94,14 @@ rhetmapschools <- rhetmapschools[order(rhetmapschools)]
 rhetmaponly <- rhetmapschools[-which(rhetmapschools %in% conschools)]
 
 # schools in consortium but not rhetmap  (1)
-consortsonly <- conschools[-which(conschools %in% rhetmapschools)]     
+consortsonly <- conschools[-which(conschools %in% rhetmapschools)]
 
 # verify departmental information, so we can know what's really produced by a consortium or rhetmap school
 source(file="update realconsorts.R")
 if(update_realconsorts) {
     noexcludes <- realconsorts_by_list("noexcludes") # use default
 }
-    
+
 # noexcludes <- realconsorts_by_list("noexcludes", manual_file = file.path(dataloc, "unconfirmed_consorts_2018-07-05.csv"))
 
 # get tag index columns on their own, for simplicity down the road
@@ -124,7 +124,7 @@ rhetmaps.index <- which(noexcludes$School %in% rhetmapschools)
 rhetmaps <- noexcludes[rhetmaps.index,]
 rhetmap.count <- nrow(rhetmaps)
 
-# so much for schools. what about actual programs? 
+# so much for schools. what about actual programs?
 realconsorts.index <- which(noexcludes$realconsort == 1)
 realconsorts <- noexcludes[realconsorts.index,]
 realconsort.count <- nrow(realconsorts)
@@ -154,11 +154,11 @@ message("Of ", consort.count, " dissertations at Consortium schools, ",
 message("Of ", rhetmap.count, " dissertations at Rhetmap schools, ",
         realrhetmap.count, " are confirmed from Rhetmap programs, ",
         "and ", length(which(rhetmaps$realrhetmap == 0)), " confirmed to be otherwise.")
-message("This brings the total count confirmed from known programs to ", nrow(knownprograms), 
+message("This brings the total count confirmed from known programs to ", nrow(knownprograms),
         " (accessible with the variable `knownprograms`), ",
         "with ", unknownprograms.count, " programs at these schools (`unknownprograms`) ",
         "remaining to be tracked down.")
-message("The total number of dissertations confirmed to be at non-RCWS programs is ", 
+message("The total number of dissertations confirmed to be at non-RCWS programs is ",
         nrow(nonrcws), "; these can be accessed with the variable `nonrcws`.")
 
 
@@ -194,23 +194,23 @@ rm("until", "since")
 new_noexcludes <- noexcludes[-which(is.na(noexcludes$Link)), ]
 
 # five-year bins
-subset_by_year <- function(dataset_name, 
-                           start_year, 
+subset_by_year <- function(dataset_name,
+                           start_year,
                            end_year,
                            autoskip = FALSE,
                            autooverwrite = FALSE) {
-    
+
     dataset <- get(dataset_name)
     new_subset_name <- paste0(dataset_name, start_year, "_", end_year)
     if(autooverwrite || !exists(new_subset_name)) {
-        assign(new_subset_name, 
+        assign(new_subset_name,
            dataset[which(dataset$Year %in% seq(start_year, end_year, 1)), ],
            envir= .GlobalEnv)
     } else if (autoskip) {
         message(new_subset_name, "already exists; autoskipping.")
         return()
     } else {
-        message(new_subset_name, " already exists with ", 
+        message(new_subset_name, " already exists with ",
                 nrow(get(new_subset_name)), " rows. (O)verwrite or (S)kip?")
         a <- readline(" ")
         while(!(tolower(a) %in% c("o", "s"))) {
@@ -218,7 +218,7 @@ subset_by_year <- function(dataset_name,
             a <- readline(" ")
         }
         if(tolower(a) == "o") {
-            assign(new_subset_name, 
+            assign(new_subset_name,
                    dataset[which(dataset$Year %in% seq(start_year, end_year, 1)), ],
                    envir= .GlobalEnv)
         } else if (tolower(a) == "s") {
@@ -227,17 +227,17 @@ subset_by_year <- function(dataset_name,
         }
     }
     message(new_subset_name, " created with ", nrow(get(new_subset_name)), " rows.")
-    
-    # make sure factor levels are limited to the subsets; 
-    # the function `refactor.all()` is defined in `Factor-Bug fixing.R` 
+
+    # make sure factor levels are limited to the subsets;
+    # the function `refactor.all()` is defined in `Factor-Bug fixing.R`
     # and sourced in `dataprep.R`
     assign(new_subset_name, refactor.all(new_subset_name), envir= .GlobalEnv)
     message("Factors in ", new_subset_name, " have been re-leveled.")
-    
+
     invisible(get(new_subset_name))
 }
 
-subset_list <- c("noexcludes", "knownprograms", "top.nonconsorts", 
+subset_list <- c("noexcludes", "knownprograms", "top.nonconsorts",
                  "realconsorts", "realrhetmaps", "nonrcws")
 # 5 year bins for everybody
 for(subset in subset_list) {
@@ -247,6 +247,23 @@ for(subset in subset_list) {
     subset_by_year(subset, 2011, 2015, autooverwrite = T)
 }
 
+# remove disses with bad.topics in the top slot (usually bad OCR or entirely in another language)
+source(file = "variation of topic proportions.R")
+message("Getting top topics so we can purge disses with bad top topics; this'll just be a minute...")
+# system.time(
+get_top_topics(dataset_name="noexcludes2001_2015",
+               ntopics = 50,
+               iter_index = 1,
+               subset_name = NULL)  # produces bad.on.top as a side effect. slow (1-2 min on laptop)
+# )
+badtops_index <- which(nonrcws2001_2015$Pub.number %in% bad.on.top)
+if (length(badtops_index)) {
+    nonrcws2001_2015sans_badtops <- nonrcws2001_2015[-badtops_index,]
+} else {
+    nonrcws2001_2015sans_badtops <- nonrcws2001_2015
+}
+
+
 # 1 year bins for knownprograms... and nonrcws
 for(startyear in 2001:2015) {
     endyear <- startyear
@@ -255,22 +272,10 @@ for(startyear in 2001:2015) {
 }
 
 
-
-# remove disses with bad.topics in the top slot (usually bad OCR or entirely in another language)
-source(file = "variation of topic proportions.R")
-# system.time(
-get_top_topics(dataset_name="noexcludes2001_2015",
-               ntopics = 50,
-               iter_index = 1,
-               subset_name = NULL)  # produces bad.on.top as a side effect. slow (1-2 min on laptop)
-# )
-nonrcws2001_2015sans_badtops <- nonrcws2001_2015[-which(nonrcws2001_2015$Pub.number %in% bad.on.top),]
-
-
 # re-factor all factor columns in all data subsets
 ## TO DO: use loops and assign() to *build* these subsets with less redundancy
 
-subset_list <- c("consorts", "nonconsorts", "realconsorts", "top.nonconsorts", "consorts.plus", 
+subset_list <- c("consorts", "nonconsorts", "realconsorts", "top.nonconsorts", "consorts.plus",
                  "noexcludes2001_2015", "consorts2001_2015", "realconsorts2001_2015", "nonconsorts2001_2015",
                  "noexcludes2001_2005", "realconsorts2001_2005", "realrhetmaps2001_2005",
                  "noexcludes2006_2010", "realconsorts2006_2010", "realrhetmaps2006_2010",
@@ -329,4 +334,3 @@ if(remake_figs || update_realconsorts) {
 
 # if "function scratchpad.R" is being used, clean up unneeded variables
 # rm(datawrangle, best_sort, filter_data, get_tags, matcharray, reorder_ser, rowdiff, rowmatch, shuffle, tagdiff, tagmatch, refactor.index)
-
